4147b77405c5269fa8b5f9012399063fd6a43648
Simon Egli <simon.egli@gmail.com>
2013-08-19 17:13:23 +0200

    updates in view.html, base.html and parser (xmltomf.py)

--- dbscript.sql
+++ dbscript.sql
@@ -144,9 +144,11 @@ CREATE TABLE localparam (
 	over float default 0.1,
     primary key (idlocal)
 );
-insert into master (FontName,FontNameA,FontNameB,idglobal) Values ("My First Metapolator Font", "FoxtailA.ufo","FoxtailB.ufo",1);
+insert into master (FontName,FontNameA,FontNameB,idglobal) Values ("Foxtail", "FoxtailA.ufo","FoxtailA.ufo",1);
+insert into master (FontName,FontNameA,FontNameB,idglobal) Values ("My First Metapolator Font", "GaramondSansA.ufo","GaramondSansB.ufo",2);
+insert into master (FontName,FontNameA,FontNameB,idglobal) Values ("Generic Sans", "SeanA.ufo","SeanB.ufo",3);
 insert into globalparam (idglobal,metapolation,unitwidth,fontsize) Values (1, 0,1,10);
-insert into globalparam (idglobal,metapolation,unitwidth,fontsize) Values (2, 0.5,1,10);
+insert into globalparam (idglobal,metapolation,unitwidth,fontsize) Values (2, 1,1,10);
 insert into localparam (idlocal) values (1);
 insert into localparam (idlocal) values (2);
 insert into localparam (idlocal) values (3);
