beec6a378f5502ebdaad076485fcb3c0e54d3499
Eric Schrijver <eric@ericschrijver.nl>
2013-08-25 18:53:56 +0200

    Sfnt2woff and ttf2eot both run from $PATH
    To not make assumptions where ttf2eot is located.

--- makefont.sh
+++ makefont.sh
@@ -7,7 +7,7 @@ rm font.afm
 
 perl mf2pt1.pl --encoding=mtp.enc --comment="Copyright (c) 2013" --family="font" --nofixedpitch --fullname="font" --name="font-regular" --weight="regular" $1
 
-sfnt2woff font.otf && ./ttf2eot font.ttf > font.eot
+sfnt2woff font.otf && ttf2eot font.ttf > font.eot
 sfnt2woff font.otf > font.woff
 
 mv font.eot static/font-webfont.eot
