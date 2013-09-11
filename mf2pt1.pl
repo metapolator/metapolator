f24b2b5dfc66843a52123e918549732a14e8aa87
Simon Egli <simon.egli@gmail.com>
2013-09-11 18:20:57 +0200

    source of mf2pt1 to install locally

--- mf2pt1.pl
+++ mf2pt1.pl
@@ -909,12 +909,7 @@ CorrectDirection();
 Simplify(0, 2);
 RoundToInt();
 AutoHint();
-foreach
-  SetCharName(NameFromUnicode(GlyphInfo("Unicode")))
-endloop
 Generate($1);
-Generate($1:r + ".otf");
-Generate($1:r + ".ttf");
 Quit(0);
 AUTOHINT
     ;
