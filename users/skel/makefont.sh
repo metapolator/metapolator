75cdae0b2091763e43dc9db7993f7175442a07ae
Simon Egli <simon.egli@gmail.com>
2013-12-11 14:34:26 -0800

    testing .otf only

--- users/skel/makefont.sh
+++ users/skel/makefont.sh
@@ -3,12 +3,12 @@ rm $1.log
 
 perl mf2pt1.pl --encoding=mtp.enc --comment="Copyright (c) 2013" --family="font" --nofixedpitch --fullname="font" --name="font-regular" --weight="regular" $1.mf
 
-sfnt2woff $1.otf && ttf2eot $1.ttf > $1.eot
-sfnt2woff $1.otf > $1.woff
+# sfnt2woff $1.otf && ttf2eot $1.ttf > $1.eot
+# sfnt2woff $1.otf > $1.woff
 
-mv $1.eot static/$1-webfont.eot
-mv $1.woff static/$1-webfont.woff
-mv $1.ttf static/$1-webfont.ttf
+# mv $1.eot static/$1-webfont.eot
+# mv $1.woff static/$1-webfont.woff
+# mv $1.ttf static/$1-webfont.ttf
 mv $1.otf static/$1.otf
 
 rm -rf $1.afm
