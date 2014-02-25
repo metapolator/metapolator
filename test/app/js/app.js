d419a71d16db53f939a07ea59bd5f97b09024308
troush <troush69@gmail.com>
2014-02-25 20:20:27 +0200

    working Inheritance for canvas cntrl

--- test/app/js/app.js
+++ test/app/js/app.js
@@ -1,9 +1,5 @@
 var metapolatortestApp = angular.module('metapolatortestApp', []);
 
-function formatter(value) {
-
-}
-
 metapolatortestApp.directive('fontSlider', function(){
     return {
         controller: 'canvasCtrl',
@@ -21,50 +17,137 @@ metapolatortestApp.directive('fontSlider', function(){
 });
 
 
-metapolatortestApp.controller('SliderCtrl', ['$scope','instancesListService', function($scope, instancesListService){
-    $scope.sliders = [
-        {
-            name:'width',
-            id:'sliderAB',
-            min: 0,
-            max: 1,
-            step:0.01,
-            value:0.2,
-            selection:'after',
-            formatter: function(value){
-                return Number(value).toFixed(2);
+metapolatortestApp.controller('SliderCtrl', ['$scope',
+                                             'instancesListService',
+    function($scope, instancesListService){
+        $scope.sliders = [
+            {
+                name:'width',
+                id:'sliderAB',
+                min: 0,
+                max: 1,
+                step:0.01,
+                value:0.2,
+                selection:'after',
+                formatter: function(value){
+                    return Number(value).toFixed(2);
+                }
             }
-        }
-    ];
-    instancesListService.addInstance('Something1');
+        ];
 }]);
 
-metapolatortestApp.controller('canvasCtrl', ['$scope','instancesListService', function($scope, instancesListService) {
-        $scope.canvasInstances = {
-        canvas: '#glyph',
-        slider: '#sliderAB',
-        fontslist: [
-        'app/fonts/RobotoSlab_Thin.otf',
-        'app/fonts/RobotoSlab_Bold.otf',
-        'app/fonts/RobotoSlab_Thin.otf'
-        ],
-        text: 'a',
-        fontSize: 400,
-        lineHeight: 215,
-        interpolationValueAB: 0.2,
-        };
-    $scope.item = 'a';
-    var glyphInstance = instancesListService.createInstance($scope.canvasInstances);
-    if (glyphInstance.loaded()){
-        glyphInstance.interpolate();
-    }
-    $scope.$watchCollection('canvasInstances', function(newVal, oldVal){
-        if (glyphInstance.loaded()) {
-            glyphInstance.interpolationValueAB = newVal.interpolationValueAB;
-            glyphInstance.interpolate();
+metapolatortestApp.controller('canvasCtrl', ['$scope',
+                                            'instancesListService',
+    function($scope, instancesListService) {
+        $scope.canvasConfig = {};
+        $scope.init = function (model) {
+                var instanceObj = instancesListService.createInstance(model);
+                if (instanceObj.loaded()){
+                    instanceObj.interpolate();
+                }
+            return instanceObj;
         }
-    }, true);
-}]);
+        $scope.watchConf = function(instanceObj, newVal){
+                if (instanceObj.loaded()) {
+                    instanceObj = newVal;
+                    instanceObj.interpolate();
+                }
+            }
+    }]);
+
+metapolatortestApp.controller('glyphCtrl', ['$scope',
+                                            'instancesListService',
+    function($scope, instancesListService) {
+        $scope.glyphConfig = {
+                canvas: '#glyph',
+                slider: '#sliderAB',
+                fontslist: [
+                'app/fonts/RobotoSlab_Thin.otf',
+                'app/fonts/RobotoSlab_Bold.otf',
+                'app/fonts/RobotoSlab_Thin.otf'
+                ],
+                text: 'a',
+                fontSize: 400,
+                lineHeight: 215,
+                interpolationValueAB: 0.2,
+            };
+        console.log($scope);
+        var glyphInstance = $scope.init($scope.glyphConfig);
+        $scope.$watchCollection('glyphConfig', function(newVal, oldVal){
+            $scope.watchConf(glyphInstance, newVal);
+        });
+    }]);
+metapolatortestApp.controller('wordCtrl', ['$scope',
+                                            'instancesListService',
+    function($scope, instancesListService) {
+        $scope.glyphConfig = {
+            canvas: '#glyphsWord',
+            slider: '#sliderAB',
+            fontslist: [
+                'app/fonts/Roboto-Regular.otf',
+                'app/fonts/Roboto-Bold.otf',
+                'app/fonts/Roboto-Regular-space.otf'
+            ],
+            text: 'Hanna',
+            fontSize: 80,
+            lineHeight: 110,
+            interpolationValueAB: 0.2,
+        };
+        console.log($scope);
+        var glyphInstance = $scope.init($scope.glyphConfig);
+        $scope.$watchCollection('glyphConfig', function(newVal, oldVal){
+            $scope.watchConf(glyphInstance, newVal);
+        });
+    }]);
+
+metapolatortestApp.controller('wordCtrl', ['$scope',
+    'instancesListService',
+    function($scope, instancesListService) {
+        $scope.glyphConfig = {
+            canvas: '#glyphsWord',
+            slider: '#sliderAB',
+            fontslist: [
+                'app/fonts/Roboto-Regular.otf',
+                'app/fonts/Roboto-Bold.otf',
+                'app/fonts/Roboto-Regular-space.otf'
+            ],
+            text: 'Hanna',
+            fontSize: 80,
+            lineHeight: 110,
+            interpolationValueAB: 0.2,
+        };
+        console.log($scope);
+        var glyphInstance = $scope.init($scope.glyphConfig);
+        $scope.$watchCollection('glyphConfig', function(newVal, oldVal){
+            $scope.watchConf(glyphInstance, newVal);
+        });
+    }]);
+
+metapolatortestApp.controller('paragraphCtrl', ['$scope',
+                                                'instancesListService',
+    function($scope, instancesListService) {
+        $scope.glyphConfig = {
+            canvas: '#paragraphWord',
+            slider: '#sliderAB',
+            fontslist: [
+                'app/fonts/RobotoSlab_Thin.otf',
+                'app/fonts/RobotoSlab_Bold.otf',
+                'app/fonts/RobotoSlab_Thin_Space.otf'
+            ],
+            text: 'Donald Ervin Knuth (born January 10, 1938) is an American computer scientist, mathematician, and Professor Emeritus at Stanford University. He is the author of the multi-volume work The Art of Computer Programming. Knuth has been called the "father" of the analysis of algorithms. He contributed to the development of the rigorous analysis of the computational complexity of algorithms and systematized formal mathematical techniques for it. In the process he also popularized the asymptotic notation. In addition to fundamental contributions in several branches of theoretical computer science, Knuth is the creator of the TeX computer typesetting system, the related METAFONT font definition language and rendering system, and the Computer Modern family of typefaces.',
+            fontSize: 20,
+            lineHeight: 32,
+            width: 690,
+            height: 400,
+            linebreaks: true,
+            interpolationValueAB: 0.2,
+        };
+        console.log($scope);
+        var glyphInstance = $scope.init($scope.glyphConfig);
+        $scope.$watchCollection('glyphConfig', function(newVal, oldVal){
+            $scope.watchConf(glyphInstance, newVal);
+        });
+    }]);
 
 metapolatortestApp.service('instancesListService', function(){
    var list = [];
