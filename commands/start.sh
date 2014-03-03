3326b7cf14716b900b7c56f278a9f477fe43e3af
hash3g <hash.3g@gmail.com>
2014-03-03 10:44:31 +0200

    pushed to repo pablo fonts to compare them with 0.5 metapolation of A and B

--- commands/start.sh
+++ commands/start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 python ./commands/metapolator.py \
-  --axis   "name:foo|A.ufo:B.ufo|koef=0" \
-  --axis   "name:bar|B.ufo:C.ufo|koef=1,metap=1" \
+  --axis   "name:foo|A.ufo:B.ufo|koef=1,metap=0.5" \
+  --axis   "name:bar|B.ufo:C.ufo|koef=0,metap=0" \
   --family "EncodeNormal-Beta70" \
   --style "400 Regular" \
   I.ufo
+
+meld ./commands/Pablo/rounded.ufo/glyphs ./commands/fontbox.ufo/glyphs
