c7435e4c5668ad992910666cfdb9ac1965e8dd7c
hash3g <hash.3g@gmail.com>
2014-03-19 17:58:06 +0200

    moved @impallari font for testing to inside `fontbox` directory

--- commands/start.sh
+++ commands/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 python ./commands/metapolator.py \
-  --axis   "name:foo|anchor.ufo:anchor.ufo|coefficient=1,metapolation=0" \
-  --axis   "name:bar|anchor.ufo:anchor.ufo|coefficient=1,metapolation=0" \
+  --axis   "name:foo|Pablo/norounding.ufo:Pablo/norounding.ufo|coefficient=1,metapolation=0" \
+  --axis   "name:bar|Pablo/norounding.ufo:Pablo/norounding.ufo|coefficient=1,metapolation=0" \
   --family "EncodeNormal-Beta70" \
   --style "400 Regular" \
   I.ufo
