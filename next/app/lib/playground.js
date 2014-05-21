3a4a412515375db909c7f75bc208e359ecbaf938
Lasse Fister <commander@graphicore.de>
2014-05-21 04:36:21 +0200

    updated sample a bit

--- next/app/lib/playground.js
+++ next/app/lib/playground.js
@@ -26,15 +26,16 @@ require([
 ) {
     "use strict";
     domReady(function() {
-        var css = "body{ \n background-color: #fff;\n }\
+        var css = "body{ \n  background-color: #fff;\n\
+  margin: 0 auto\n}\
 /* And a comment */ \
 strange>.selector\
 /* selector comment \n with linebreak */\
 , and another one\
 missing.delim >\
-{ any-param: unheard-of(style + css);\
-/* a comment within a declaration */\
-another-one: def-unheard-of(style + css);/*this param has a comment*/\
+{ any-param\n: unheard-of(style + css);\
+/* a comment within a block */\
+another-one: def-unheard-of(style + css)/*this param has a comment*/;\
 }\
 @media whatever{ hi{name:val} }\
 ";
