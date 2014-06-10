db06a2eee9736a6cd2599a0561ccf37c600735a8
Lasse Fister <commander@graphicore.de>
2014-06-10 18:55:33 +0200

    initial cli structure for the importer

---
+++ next/app/lib/importer/cli.js
@@ -0,0 +1,13 @@
+define([
+    'obtain/obtain'
+  , 'ufojs/main'
+], function (
+    obtain
+  , ufojs
+) {
+    "use strict";
+    function main(args) {
+        console.log('import main!');
+    }
+    return {main: main};
+})
