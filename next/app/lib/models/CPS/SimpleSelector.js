1571c57ff62ce61ed39ee52a153801e7281d4b7a
Lasse Fister <commander@graphicore.de>
2014-06-22 23:51:01 +0200

    parse name, and value if pseudoclas

--- next/app/lib/models/CPS/SimpleSelector.js
+++ next/app/lib/models/CPS/SimpleSelector.js
@@ -30,13 +30,26 @@ define([
         if(!(element instanceof GenericCPSNode))
             throw new CPSError('Element has the wrong type: "' + element
                                 + '" should be a GenericCPSNode.')
-        this._type = this._getType(element);
+        this._type = this._getType();
         if(this._type === undefined) {
             this._alien = true;
-            this._message = 'Type of simple selector is unsupported: ' + element
+            this._message = 'Type of SimpleSelector is unsupported: ' + element;
         }
-        // todo: process the values. especially for items where the value
-        // is  'function'
+        this._name = this._getName();
+        if(this._name === undefined) {
+            this._invalid = true;
+            this._message = 'Name of SimpleSelector is unkown: ' + element;
+            return;
+        }
+        
+        if(this._type === 'pseudo-class' && this._name === 'i') {
+            this._value = this._getPseudoClassValueForIndex();
+            if(this._value === undefined) {
+                this._invalid = true;
+                this._message = 'Can\'t get value for pseudoclass "i": ' + element;
+                return;
+            }
+        }
     }
     
     var _p = SimpleSelector.prototype = Object.create(Parent.prototype)
@@ -58,10 +71,17 @@ define([
     Object.defineProperty(_p, 'type', {
         get: function(){ return this._type;}
     });
-    _p._getType = function(element) {
-        switch(element.type){
+    Object.defineProperty(_p, 'name', {
+        get: function(){ return this._name;}
+    });
+    Object.defineProperty(_p, 'value', {
+        get: function(){ return this._value;}
+    });
+    
+    _p._getType = function() {
+        switch(this._element.type){
           case 'ident':
-            if(element._ast[1] === '*')
+            if(this._element._ast[1] === '*')
                 return 'universal';
             return 'type';
           case 'clazz':
@@ -76,6 +96,43 @@ define([
             return 'pseudo-element';
         }
         return undefined;
+    }
+    
+    _p._getName = function() {
+        var name = name;
+        if(typeof this._element._ast[1] === 'string') {
+            name = this._element._ast[1];
+        }
+        else if(this._element._ast[1] instanceof Array) {
+            if(this._element._ast[1][0] === 'ident')
+                name = this._element._ast[1][1];
+            else if(this._element._ast[1][0] === 'funktion'
+                    && this._element._ast[1][1] instanceof Array
+                    && this._element._ast[1][1][0] === 'ident')
+                name = this._element._ast[1][1][1];
+        }
+        if(typeof name !== 'string' && name !== undefined)
+            throw new CPSError('Can\'t find a name for SimpleSelector (' 
+                            + this._element + ')')
+        return name;
+    }
+    
+    _p._getPseudoClassValueForIndex = function(){
+        console.log('_getPseudoClassValueForIndex', this._element._ast[1]);
+        if(this._element._ast[1][0] !== 'funktion'
+                || this._element._ast[1][2][0] !== 'functionBody')
+            return;
+        var body
+          , number
+          ;
+        body = this._element._ast[1][2].slice(1)
+            .filter(function(item) {
+                return !(item[0] in {'s':null,'comment':null});
+            })
+        if(body.length === 1 && body[0][0] === 'number')
+            number = parseInt(body[0][1], 10);
+            // if the result is NaN return undefined
+            return (number === number) ? number : undefined;
     }
     
     return SimpleSelector;
