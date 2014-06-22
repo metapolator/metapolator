0c6742722f0b528fa10af000f1a15f4bf213f750
Lasse Fister <commander@graphicore.de>
2014-06-22 23:59:10 +0200

    added common selector apis: selects, invalid, alien, message, value

--- next/app/lib/models/CPS/SelectorList.js
+++ next/app/lib/models/CPS/SelectorList.js
@@ -5,13 +5,30 @@ define([
 ) {
     "use strict";
     /**
-     * A list of selectors
+     * A list of ComplxSelectors
      */
-    function SelectorList(selectors, source, lineNo){
+    function SelectorList(selectors, source, lineNo) {
         Parent.call(this, source, lineNo);
         this._selectors = [];
         if(selectors.length)
             this.push.apply(this, selectors);
+        
+        // Maybe we'll need to determine the following on demand, not on init??
+        var i=0, count=0;
+        for(;i<this.selectors.length;i++) {
+            if(this._selectors[i].invalid) {
+                this._invalid = true;
+                this._message = this._selectors[i].message;
+                break;
+            }
+            if(!this._selectors[i].selects)
+                continue;
+            count +=1;
+        }
+        if(!count) {
+            this._invalid = true;
+            this._message = 'SelectorList has no selecting selector';
+        }
     }
     var _p = SelectorList.prototype = Object.create(Parent.prototype)
     _p.constructor = SelectorList;
@@ -27,6 +44,27 @@ define([
     Object.defineProperty(_p, 'length', {
         get: function(){ return this._selectors.length }
     })
+    
+    Object.defineProperty(_p, 'selects', {
+        get: function(){ return !this._invalid; }
+    });
+    Object.defineProperty(_p, 'invalid', {
+        get: function(){ return this._invalid;}
+    });
+    Object.defineProperty(_p, 'alien', {
+        get: function(){ return this._alien;}
+    });
+    Object.defineProperty(_p, 'message', {
+        get: function(){ return this._message;}
+    });
+    
+    var _filterSelecting = function(selector) {
+        return selector.selects;
+    }
+    Object.defineProperty(_p, 'value', {
+        get: function(){ return this._selectors.filter(_filterSelecting); }
+    });
+    
     
     /**
      * Add selectors to this SelectorList.
