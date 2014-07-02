d65c85219e498bc42e8db9123bde312174860be9
Lasse Fister <commander@graphicore.de>
2014-07-02 16:31:49 +0200

    styledict knows its controller, refactored caching

--- next/app/lib/models/CPS/StyleDict.js
+++ next/app/lib/models/CPS/StyleDict.js
@@ -5,81 +5,84 @@ define([
 ) {
     "use strict";
     
+    var KeyError = errors.Key;
+    
     /**
-     * StyleDict a interface to a List of CPS.Rule elements.
-     * This is returned by the getComputedStyle function of the
-     * selector engine (currently located in ParameterCollection)
+     * StyleDict is an interface to a List of CPS.Rule elements.
+     * 
+     * This is returned by the getComputedStyle function of StyleControler.
      */
-     
-    function StyleDict(parameterRegistry, rules, element) {
+    function StyleDict(controller, rules, element) {
         this._rules = rules;
         this._element = element;
-        // global registry for all known parameter names
-        this._parameterRegistry = parameterRegistry;
+        this._controller = controller;
+        this._cache = {};
     }
     
     var _p = StyleDict.prototype;
     _p.constructor = StyleDict;
     
-    /**
-     * get a value instance for the element
-     */
-    _p.get = function(name) {
-        // this method could be a good fit for the element directly
-        // or for a 'document' like structure that keeps some of the
-        // state data separatet from the MOM elements. The latter would
-        // make it possible to change the CPS without having to create
-        // a new instance of the mom!
-        
+    
+    _p._get = function(name) {
         var cpsValue, factory, result;
         cpsValue = this.getCPS(name);
-        factory = cpsValue === undefined
+        factory = cpsValue === null
             // Note: An element default value, if needed, should be defined
             // in a very unspecific rule in a very unspecific style
             // (i.e.: global.css)
             // The global parameter default must always be available:
-            ? this._parameterRegistry.getDefaultFactory(name)
+            ? this._controller.parameterRegistry.getDefaultFactory(name)
             : cpsValue.factory
             ;
-        
+        return factory(name, this._element, this.getCPSValueAPI.bind(this));
+    }
+    
+    /**
+     * get a value instance for the element
+     * 
+     * this method could be a good fit for the element directly
+     * or for a 'document' like structure that keeps some of the
+     * state data separatet from the MOM elements. The latter would
+     * make it possible to change the CPS without having to create
+     * a new instance of the mom!
+     */
+    _p.get = function(name) {
         // this is a caching mechanism, this might be harmful, because we
         // create a cache that needs invalidation from time to time.
         // Not caching the value means that we get a different instance
         // for each call to this function, not good either. Maybe the
         // position of the cache could change.
-        if(!(name in this._element._parameterInstances)) {
-            result = factory(name, this._element, this.getCPSValueAPI.bind(this));
-            this._element._parameterInstances[name] = result;
-            return result;
-        }
-        
-        return this._element._parameterInstances[name];
+        if(!(name in this._cache))
+            this._cache[name] = this._get(name);
+        return this._cache[name];
     }
     
     _p.getCPSValueAPI = function(name) {
-        var result;
-        if(this._parameterRegistry.exists(name))
-            // the local value wins over any refernced entry from @dictionary
-            result = this.get(name);
+        if(this._controller.parameterRegistry.exists(name))
+            // the name is a registered parameter, so it always has at
+            // least a default value
+            return this.get(name);
         
-        return result;
+        // get the dictionary StyleDict for element
+        console.log('search in @dictionary');
+        throw new KeyError('Not found CPS Value "'+ name+'" for: '
+            + this._element.particulars);
     }
-    
     
     /**
      *  get a cps ParameterValue from the _rules
      */
     _p.getCPS = function(name) {
         var i=0, value;
-        if(!this._parameterRegistry.exists(name))
-            throw new errors.Key('Name "'+ name + '" is no known parameter, '
+        if(!this._controller.parameterRegistry.exists(name))
+            throw new KeyError('Name "'+ name + '" is no known parameter, '
                 +'it must be registered before you can use it.');
         for(;i<this._rules.length;i++) {
             try {
                 value = this._rules[i].parameters.get(name);
             }
             catch(error) {
-                if(!(error instanceof errors.Key))
+                if(!(error instanceof KeyError))
                     throw error;
                 // pass, the name is not in the rule
                 continue;
@@ -87,6 +90,7 @@ define([
             if(!value.invalid)
                 return value;
         }
+        return null;
     }
     
     return StyleDict;
