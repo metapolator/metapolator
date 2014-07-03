9a99b93fbe4fa154d04ae89ba80bd817f2116a65
Lasse Fister <commander@graphicore.de>
2014-07-03 08:51:24 +0200

    allow name to be set after construction

--- next/app/lib/models/CPS/elements/AtRuleCollection.js
+++ next/app/lib/models/CPS/elements/AtRuleCollection.js
@@ -1,11 +1,12 @@
 define([
     'metapolator/errors'
   , './_Collection'
+  , './AtRuleName'
 ], function(
     errors
   , Parent
+  , AtRuleName
 ) {
-    console.log('hi', Parent)
     "use strict";
     var CPSError = errors.CPS;
     /**
@@ -13,13 +14,34 @@ define([
      */
     function AtRuleCollection(name, items, source, lineNo) {
         Parent.call(this, items, source, lineNo);
-        this._name = name;
+        if(name)
+            this._name = name;
     }
     var _p = AtRuleCollection.prototype = Object.create(Parent.prototype)
     _p.constructor = AtRuleCollection;
     
+    /**
+     * Due to the way the parsing is made, name can be set after
+     * initialisation but only once.
+     * 
+     * If name is not set, trying to read the name raises an CPSError
+     */
     Object.defineProperty(_p, 'name', {
-        get: function(){return this._name; }
+        set: function(name) {
+            if(this._name)
+                throw new CPSError('Name is already set');
+            if(!name instanceof AtRuleName)
+                throw new CPSError('Name has the wrong type, expected '
+                    + 'AtRuleName but got: ' + name.constructor.name
+                        ? name.constructor.name
+                        : name.constructor);
+            this._name = name;
+        }
+      , get: function(){
+            if(!this._name)
+                throw new CPSError('Name not set yet.');
+            return this._name;
+        }
     })
     
     return AtRuleCollection;
