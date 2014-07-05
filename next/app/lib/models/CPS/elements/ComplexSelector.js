8d8e9a875cfaf4da390a7ba531b9094266a36a66
Lasse Fister <commander@graphicore.de>
2014-07-05 22:31:06 +0200

    remove AST parsing, keep validation

--- next/app/lib/models/CPS/elements/ComplexSelector.js
+++ next/app/lib/models/CPS/elements/ComplexSelector.js
@@ -1,17 +1,11 @@
 define([
     'metapolator/errors'
   , './_Node'
-  , './GenericCPSNode'
-  , './Comment'
   , './Combinator'
-  , './CompoundSelector'
 ], function(
     errors
   , Parent
-  , GenericCPSNode
-  , Comment
   , Combinator
-  , CompoundSelector
 ) {
     "use strict";
     
@@ -50,25 +44,81 @@ define([
      * 
      * 
      */
-    function ComplexSelector(elements, source, lineNo) {
+    function ComplexSelector(value, source, lineNo) {
         Parent.call(this, source, lineNo);
-        this._elements = elements;
+        this._value = value;
+        this._invalid = false;
+        this._alien = false;
+        this._message = undefined;
         
-        var status = this._parseSelector(elements);
+        var i = 0
+          , item
+          ;
         
-        this._invalid = status.invalid || false;
-        this._alien = status.alien || false;
-        this._message = status.message || undefined;
-        this._value = status.value || undefined;
+        for(;i<this._value.length;i++) {
+            item = value[i];
+            if(item instanceof Combinator) {
+                if(!(item.type in this._combinators)) {
+                    this._alien = true;
+                    this._message = 'The combinator type "'+ item.type
+                            +'" is alien to us';
+                }
+                // keep on parsing, this can still become invalid
+                if(i === 0) {
+                    this._invalid = true;
+                    this._message = 'The first item in a selector must '
+                                                +'not be a combinator';
+                    return;
+                }
+                // if this is a combinator and the last item in values
+                // is a combinator, too, this is inavlid
+                else if(this._value[this._value.length-1] instanceof Combinator) {
+                    this._invalid = true;
+                    this._message = ['Two subsequent combinators in a'
+                                    , 'selector are not valid. Found:'
+                                    , this._value[this._value.length-1].value
+                                    , 'followed by:' + item.value
+                                    ].join(' ');
+                    return;
+                }
+            }
+            else if(item.alien || item.invalid) {
+               this._invalid = item.invalid || this._invalid;
+               this._alien = item.alien || this._alien;
+               this._message = item.message;
+               return;
+            }
+        }
+        if(!this._value.length) {
+            this._invalid = true;
+            this._message =  'A selector must not be empty'
+        } 
+        else if(this._value[this._value.length-1] instanceof Combinator) {
+                this._invalid = true;
+                this._message ='The last item of a selector must not '
+                                +'be a combinator, found: '
+                                + this._value[this._value.length-1].value;
+        }
+    }
+    
+    ComplexSelector.add = function(a, b) {
+        var value = a.value;
+        value.push(new Combinator(' ', a.source, a.lineNo));
+        Array.prototype.push.apply(value, b.value);
+        return new ComplexSelector(value, a.source, a.lineNo);
     }
     
     var _p = ComplexSelector.prototype = Object.create(Parent.prototype)
     _p.constructor = ComplexSelector;
     
     _p.toString = function() {
-        if(this._value)
-            return this._value.join('');
-        return this._elements.join('');
+        return this._value.join('');
+    }
+    
+    _p._combinators = {
+        // the child combinator
+        'child' : true
+      , 'descendant': true
     }
     
     Object.defineProperty(_p, 'selects', {
@@ -109,124 +159,8 @@ define([
         }
     })
     
-    /* combinators that are not alien */
-    _p._combinators = {
-        // the child combinator
-        'child' : true
-        // whitespace is the descendant combinator, but we have to detect
-        // this another way
-    }
-    
-    
-    //FIXME! move this to parsing module!
-    _p._parseSelector = function(elements) {
-        var status = {
-                ivalid: false
-              , alien: false
-              , message: undefined
-              // will be set when it is valid
-              , value: undefined
-            }
-          , value = []
-          , i=0
-          , item
-          , CompoundSelectorElements = null
-          , isWhitespace
-          ;
-        
-        var util = require.nodeRequire('util');
-        for(; i<elements.length; i++) {
-            item = elements[i];
-            isWhitespace = (item instanceof GenericCPSNode && item.type === 's');
-            if(isWhitespace && value.length === 0)
-                // skip all whitespaces at the beginning
-                continue;
-            else if(item instanceof Comment)
-                // skip all comments
-                continue;
-            
-            if(item instanceof Combinator) {
-                if(!(item.type in this._combinators)) {
-                    status.alien = true;
-                    status.message = 'The combinator type "'+ item.type
-                                  +'" is alien to us';
-                }
-                // keep on parsing, this can still become invalid
-                if(value.length === 0) {
-                    status.invalid = true;
-                    status.message = 'The first item in a selector must '
-                                                +'not be a combinator';
-                    break;
-                }
-                // if this is a combinator and the last item in values is a
-                // combinator, too, this is inavlid
-                else if(value[value.length-1] instanceof Combinator) {
-                    status.invalid = true;
-                    status.message = ['Two subsequent combinators in a'
-                                     , 'selector are not valid. Found:'
-                                     , value[value.length-1].value
-                                     , 'followed by:' + item.value
-                                     ].join(' ');
-                    break;
-                }
-                // close the current simple selector
-                CompoundSelectorElements = null;
-                value.push(item);
-                continue;
-            }
-            // may be whitespace, or a simple selector
-            if(isWhitespace) {
-                // close the current simple selector
-                CompoundSelectorElements = null;
-                continue;
-            }
-            // must be a simple selector (or invalid/alien)
-            if(CompoundSelectorElements === null) {
-                // if no other combinator is already there:
-                if(value.length && !(value[value.length-1] instanceof Combinator))
-                    // push a simple 'descendant' Combinator
-                    // it's somehow pointless to use this._source, this._lineNo
-                    // in this case. we could have remembered the source and line
-                    // of the last whitespace
-                    value.push(new Combinator(' ', this._source, this._lineNo));
-                
-                // make a new one
-                CompoundSelectorElements = [];
-                value.push(CompoundSelectorElements);
-            }
-            CompoundSelectorElements.push(item);
-        }
-        
-        if(!value.length) {
-            status.invalid = true;
-            status.message = 'A selector must not be empty'
-        }
-        else if(value[value.length-1] instanceof Combinator) {
-            status.invalid = true;
-            status.message = 'The last item of a selector must not be a '
-                             +' combinator, found: ' + value[value.length-1].value
-        }
-        else {
-            // build the CompoundSelectors
-            for(i=0; i<value.length; i++) {
-                if(value[i] instanceof Combinator)
-                    continue;
-                
-                value[i] = new CompoundSelector(value[i], value[i][0]._source
-                                                    , value[i][0]._lineNo);
-                if(value[i].alien || value[i].invalid) {
-                    status.invalid = value[i].invalid || status.invalid;
-                    status.alien = value[i].alien || status.alien;
-                    status.message = value[i].message;
-                    if(status.invalid)
-                        break;
-                }
-            }
-        }
-        
-        if(!status.invalid && !status.alien)
-            status.value = value;
-        return status;
+    _p.add = function(complexSelector) {
+        return this.constructor.add(this, complexSelector);
     }
     
     return ComplexSelector;
