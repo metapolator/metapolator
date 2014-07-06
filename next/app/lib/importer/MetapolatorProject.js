be1cd27a743245342535d923108a58c59f5f87c0
Lasse Fister <commander@graphicore.de>
2014-07-06 18:22:55 +0200

    cleanup init and load

--- next/app/lib/importer/MetapolatorProject.js
+++ next/app/lib/importer/MetapolatorProject.js
@@ -43,7 +43,6 @@ define([
         var project = new MetapolatorProject(io)
           , dirName = name+'.ufo'
           ;
-          
         // create dirName
         if(io.pathExists(false, dirName))
             throw new ProjectError('Dir exists already: '+ dirName);
@@ -52,7 +51,6 @@ define([
     
     var _p = MetapolatorProject.prototype;
     _p.constructor = MetapolatorProject;
-    
     Object.defineProperty(_p, 'dataDir', {
         get: function(){ return this.dirName + '/data/com.metaploator'}
     });
@@ -73,7 +71,7 @@ define([
         get: function(){ return 'global.cps'; }
     });
     
-    Object.defineProperty(_p, 'layercontentsFile', {
+    Object.defineProperty(_p, 'layerContentsFile', {
         get: function(){ return this.dirName+'/layercontents.plist'; }
     });
     
@@ -82,7 +80,7 @@ define([
     _p.init = function(dirName) {
         // everything synchronously right now
         this.dirName = dirName;
-
+        
         this._mkdir(false, this.dirName);
         
         // create dirName/metainfo.plist
@@ -102,13 +100,13 @@ define([
         this._mkdir(false, this.cpsDir);;
         
         // create layercontents.plist
-        this._io.writeFile(false, this.layercontentsFile,
+        this._io.writeFile(false, this.layerContentsFile,
                                         plistLib.createPlistString([]));
-                            
+        
         // the glyphs dir must be there, so the ufo is valid. but we don't
         // use it currently :-(
         // create dir dirName/glyphs
-        this._createGlyphLayer('public.default',  this.dirName+'/glyphs');
+        this._createGlyphLayer('public.default', 'glyphs');
         
         // create a default.cps
         // this is the standard wiring of cps compounds etc.
@@ -116,7 +114,7 @@ define([
         // probably we don't have to save it on disk, because we can
         // generate it internally and because we don't want it to be changed
         // to override these parameters, the global.cps file is in place.
-        this._io.writeFile(false, this.cpsDefaultFile, this.getDefaultCPS());
+        // this._io.writeFile(false, this.cpsDefaultFile, this.getDefaultCPS());
         
         // this can be empty, all masters will use this by default
         this._io.writeFile(false, [this.cpsDir, '/', this.cpsGlobalFile].join(''),
@@ -134,21 +132,23 @@ define([
         // the files created in _p.init need to exist
         // however, we try to load only
         // this.dirName+'/data/com.metapolator/project.yaml' as an indicator
+        console.log('loading', this.projectFile);
         var dataString = this._io.readFile(false, this.projectFile);
+        console.log('loaded', dataString);
         this._data = yaml.safeLoad(dataString);
         
-        console.log('loaded', dataString);
+        
     }
     
     /**
      * return a ParameterCollection with the default CPS wireing, as the
      * importer expects it.
      */
-    _p._getDefaultCPS: function() {
+    _p._getDefaultCPS = function() {
         throw new errors.notImplemented('getDefaultCPS is not implemented')
     }
     
-    _p.hasMaster: function(masterName) {
+    _p.hasMaster = function(masterName) {
         return masterName in this._data.masters;
     }
     
@@ -178,7 +178,7 @@ define([
         
         // store layer in layercontents
         layercontents.push([name, layerDirName]);
-        this._io.writeFile(false, this.layercontentsFile,
+        this._io.writeFile(false, this.layerContentsFile,
                                     plistLib.createPlistString(layercontents));
         
         // create empty layerDir/contents.plist
@@ -196,21 +196,21 @@ define([
         // read layercontents.plist
         var layercontents = plistLib.readPlistFromString(
                 this._io.readFile(false, this.layerContentsFile))
-          , layerdir
+          , layerDir
           ;
         
         for(var i=0;i<layercontents.length;i++)
             if(layercontents[i][0] === name) {
-                layerdir = [this.dirName,'/',layercontents[i][1]].join('');
+                layerDir = [this.dirName,'/',layercontents[i][1]].join('');
                 break;
             }
-        if(!layerdir)
+        if(!layerDir)
             throw new KeyError('Layer named "' + name + '" not found.')
         if(!this._io.pathExists(false, layerDir))
             throw new KeyError('Layer directory "' + layerDir
                                 + '" does not exist, but is linked in '
                                 +'layercontents.plist.')
-        return layerdir;
+        return layerDir;
     }
     
     /**
@@ -224,7 +224,7 @@ define([
      * skeleton.masterName, glyphs.skeleton.masterName
      * 
      */
-    _p.createMaster: function(masterName) {
+    _p.createMaster = function(masterName) {
         // get the name for this master from the cli
         if(this.hasMaster(masterName))
             throw new ProjectError('Master "'+masterName+'" alredy exists.');
@@ -237,21 +237,24 @@ define([
         master.skeleton = 'skeleton.' + masterName;
         this._createGlyphLayer(master.skeleton);
         
+        
+        this._io.writeFile(false, this.projectFile, yaml.safeDump(this._data))
+        
         return this.getMaster(masterName);
     }
-    _p._getMaster(masterName) {
+    _p._getMaster = function(masterName) {
         var master =  this._data.masters[masterName]
-          , glyphSetDir = this._getLayerDirForMaster(master.skeleton)
+          , glyphSetDir = this._getLayerDir(master.skeleton)
           ;
         return new ProjectMaster(this._io, this, glyphSetDir
                                 , master.cpsLocalFile, master.cpsChain);
     }
     
-    _p.getMaster: function(masterName) {
+    _p.getMaster = function(masterName) {
         if(!this.hasMaster(masterName))
             throw new KeyError('Master "'+masterName+'" not in Projekt');
         if(!this._masterCache[masterName]) {
-            this._masterCache[masterName] = thsi._getMaster(masterName);
+            this._masterCache[masterName] = this._getMaster(masterName);
         }
         return this._masterCache[masterName];
     }
