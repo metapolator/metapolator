8cf26fe21259665ff2dc8ed4c14e920e35b40979
Lasse Fister <commander@graphicore.de>
2014-07-06 18:21:45 +0200

    input validation and call to importGlyph

--- next/app/lib/importer/ImportController.js
+++ next/app/lib/importer/ImportController.js
@@ -3,18 +3,28 @@ define([
   , 'ufojs/ufoLib/glifLib/GlyphSet'
   , 'metapolator/models/Controller'
   , './parameterRegistry'
+  , './readOutlines/SegmentPen'
+  , './readOutlines/ImportOutlinePen'
+  
 ], function(
     errors
   , GlyphSet
   , ModelController
   , parameterRegistry
-  
+  , SegmentPen
+  , ImportOutlinePen
 ) {
     "use strict";
     
-    function ImportController(io, project, sourceUFODir) {
+    function ImportController(io, project, masterName, sourceUFODir) {
         this._io = io;
         this._project = project;
+        
+        if(this._project.hasMaster(masterName))
+            this._master = this._project.getMaster(masterName);
+        else
+            this._master = this._project.createMaster(masterName);
+        
         // open the source ufo glyphs layer as UFOv2
         this._sourceGlyphSet = GlyphSet.factory(
             false, io,
@@ -24,18 +34,24 @@ define([
     }
     var _p = ImportController.prototype;
     
-    _p.import = function(masterName, glyphs) {
-        console.log('importing', glyphs === undefined
-            ? 'all glyphs'
-            : 'the glyphs: ' + glyphs.join(', ')
-            , '...'
-        );
+    _p.import = function(glyphs) {
+        var missing, i=0;
         
-        var master;
-        if(this._project.hasMaster(masterName));
-            master = this._project.getMaster(masterName)
-        else
-            master = this._project.createMaster(masterName)
+        if(!glyphs)
+            glyphs = this._sourceGlyphSet.keys();
+        else {
+            missing = glyphs.filter(function(name) {
+                        return !this._sourceGlyphSet.has_key(name);}, this)
+            if(missing.length)
+                throw new errors.Key('Some for import requested glyphs '
+                                    +'are missing in the source GlyphSet: '
+                                    +missing.join(', '));
+        }
+        console.log('importing: ...');
+        for(;i<glyphs.length;i++)
+            this.importGlyph(glyphs[i]);
+        
+        
     }
     
     _p._readGlyphFromSource = function(glyphName) {
@@ -45,16 +61,13 @@ define([
           ;
         
         glyph.drawPoints(false, pen);
-        contours = segmentPen.flush();
-        return {data:glyph, contours:contours};
+        return {data:glyph, contours:segmentPen.flush()};
     }
     
     _p.importGlyph = function(glyphName) {
+        console.log('> import glyph:', glyphName);
         var glyph = this._readGlyphFromSource(glyphName);
-        
-        
     }
-    
     
     return ImportController;
 });
