6b994dec3552011f0aa21b48af508e20c9760aae
troush <troush69@gmail.com>
2014-07-07 20:26:23 +0300

    lib files for glyph-inspector

---
+++ next/app/lib/zip.js/tests/test7.js
@@ -0,0 +1,18 @@
+var zipFs = new zip.fs.FS();
+
+function onerror(message) {
+	console.error(message);
+}
+
+function logText(text) {
+	console.log(text);
+	console.log("--------------");
+}
+
+zip.workerScriptsPath = "../";
+zipFs.importHttpContent("lorem.zip", false, function() {
+	var firstEntry = zipFs.root.children[0];
+	firstEntry.getText(function(data) {
+		logText(data);
+	});
+}, onerror);
