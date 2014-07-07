54a4043a102d85005d5c08f3505ef13f9ee868f5
Lasse Fister <commander@graphicore.de>
2014-07-07 18:24:35 +0200

    register the parameters that the importer uses

--- next/app/lib/importer/parameters/registry.js
+++ next/app/lib/importer/parameters/registry.js
@@ -9,39 +9,65 @@ define([
     
     var parameterRegistry = new Registry();
     
-    parameterRegistry.register('value', {
-                type: 'string'
-              , description: 'this is a stub for the parameter description!'
-    });
-    
-    parameterRegistry.register('height', {
-                type: 'compoundReal'
-              , description: 'the relative value of height'
-    });
-    
-    parameterRegistry.register('heightIntrinsic', {
-                type: 'real'
-              , description: 'the intrinsic value of the height'
-    });
-    
-    parameterRegistry.register('width', {
-                type: 'compoundReal'
-              , description: 'the relative value of width'
-    });
-    
-    parameterRegistry.register('widthIntrinsic', {
-                type: 'real'
-              , description: 'the intrinsic value of the width'
-    });
-    
-    parameterRegistry.register('zon', {
+    parameterRegistry.register('on', {
                 type: 'compoundVector'
-              , description: 'The center on curve point of a skeleton point'
+              , description: 'An On-Curve Point.'
     })
-    parameterRegistry.register('zonIntrinsic', {
+    parameterRegistry.register('onIntrinsic', {
                 type: 'vector'
-              , description: 'the intrinsic value of the zon'
+              , description: 'The intrinsic value of an On-Curve Point.'
     })
+    
+    parameterRegistry.register('in', {
+                type: 'compoundVector'
+              , description: 'An incoming Control Point.'
+    })
+    parameterRegistry.register('inIntrinsic', {
+                type: 'vector'
+              , description: 'The intrinsic value of an incoming Control Point.'
+    })
+    
+    parameterRegistry.register('out', {
+                type: 'compoundVector'
+              , description: 'An outgoing Control Point.'
+    })
+    parameterRegistry.register('outIntrinsic', {
+                type: 'vector'
+              , description: 'The intrinsic value of an outgoing Control Point.'
+    })
+    
+    
+    parameterRegistry.register('inTension', {
+                type: 'real'
+              , description: 'The tension value of an incoming Control Point.'
+    });
+    
+    parameterRegistry.register('outTension', {
+                type: 'real'
+              , description: 'The tension value of an outgoing Control Point.'
+    });
+    
+    parameterRegistry.register('inDir', {
+                type: 'compoundVector'
+              , description: 'Direction vector of an incoming Control Point.'
+    });
+    
+    parameterRegistry.register('inDirIntrinsic', {
+                type: 'vector'
+              , description: 'The intrinsic value of the direction of an '
+                                                + 'incomimg Control Point.'
+    })
+    
+    parameterRegistry.register('outDir', {
+                type: 'compoundVector'
+              , description: 'Direction vector of an outgoing Control Point.'
+    });
+    
+    parameterRegistry.register('outDirIntrinsic', {
+                type: 'vector'
+              , description: 'The intrinsic value of the direction of an '
+                                                + 'outgoing Control Point.'
+    });
     
     return parameterRegistry;
 });
