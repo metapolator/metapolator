81bc5c0631924bd3c24b0b4670b9bfc3b00e6037
Reuben Thomas <rrt@sc3d.org>
2014-08-03 19:33:25 +0100

    Make cps-selectors command take a real typeface

--- app/commands/playground/cps-selectors.js
+++ app/commands/playground/cps-selectors.js
@@ -1,37 +1,31 @@
 define([
     'metapolator/errors'
-  , 'gonzales/gonzales'
   , 'metapolator/cli/ArgumentParser'
   , 'ufojs/tools/io/staticNodeJS'
-  , 'metapolator/models/CPS/selectorEngine'
-  , 'metapolator/models/MOM/Univers'
-  , 'metapolator/models/MOM/Master'
-  , 'metapolator/models/MOM/Glyph'
-  , 'metapolator/models/MOM/PenStroke'
-  , 'metapolator/models/MOM/PenStrokePoint'
-  , 'metapolator/models/CPS/Registry'
-  , 'metapolator/models/CPS/StyleDict'
-  , 'metapolator/models/Controller'
+  , 'metapolator/project/MetapolatorProject'
 ], function (
     errors
-  , gonzales
   , ArgumentParser
   , io
-  , selectorEngine
-  , Univers
-  , Master
-  , Glyph
-  , PenStroke
-  , PenStrokePoint
-  , Registry
-  , StyleDict
-  , Controller
+  , MetapolatorProject
 ) {
     "use strict";
     var CommandLineError = errors.CommandLine
       , argumentParser = new ArgumentParser('cps-selectors')
       , module
       ;
+
+    argumentParser.addArgument(
+        'MasterName'
+      , 'The master to be exported'
+      , function(args) {
+            var name = args.pop();
+            if(name === undefined)
+                throw new CommandLineError('No MasterName argument found');
+            return name;
+        }
+    );
+
     argumentParser.addArgument(
         'selectors'
       , 'a comma separated list of selectors, best use quotation marks for shell escaping'
@@ -55,78 +49,17 @@ define([
         console.log('processed arguments', args)
         console.log('processed options', options)
         
-        var ralph = new Master()
-          , heidi = new Master()
-            /* we don't read values here, just selecting elements*/
-          , controller = new Controller(undefined)
-          , univers = controller.query('univers')
-          , mastersOfTheUnivers
-          , data = {}
+        var project
+          , controller
           ;
-        ralph.id = 'ralph';
-        heidi.id = 'heidi';
-        univers.add(ralph);
-        univers.add(heidi);
+
+        project = new MetapolatorProject(io);
+        project.load();
+        controller = project.open(args.MasterName);
+
+        console.log('selector:', args.selectors );
         
-        mastersOfTheUnivers = univers.children
-        
-        data[ralph.id] = {
-            glyphs: [
-                      ['a',[//strokes
-                        Array(3),Array(4),Array(8)
-                      ]]
-                    , ['b',[//strokes
-                        Array(4),Array(10),Array(14)
-                    ]]
-                    , ['c',[//strokes
-                        Array(12),Array(5),Array(24)
-                    ]]
-            ]
-        }
-        data[heidi.id] = {
-            glyphs: [
-                      ['x',[//strokes
-                        Array(3),Array(4),Array(8)
-                        
-                      ]]
-                    , ['y',[//strokes
-                        Array(4),Array(10),Array(14)
-                    ]]
-                    , ['z',[//strokes
-                        Array(12),Array(5),Array(24)
-                    ]]
-            ]
-            
-        }
-        
-        for(var h=0; h<mastersOfTheUnivers.length; h++) {
-            var glyphs = data[mastersOfTheUnivers[h].id].glyphs;
-            for(var i=0;i<glyphs.length;i++) {
-                var glyph = new Glyph();
-                glyph.id = glyphs[i][0];
-                mastersOfTheUnivers[h].add(glyph);
-                for(var j=0;j<glyphs[i][1].length;j++) {
-                    var stroke = new PenStroke();
-                    glyph.add(stroke);
-                    for(var k=0;k<glyphs[i][1][j].length;k++) {
-                        var point = new PenStrokePoint();
-                        stroke.add(point);
-                    }
-                }
-            }
-        }
-        
-        
-        
-        var scope = controller.query('penstroke:i(1)')
-          , result
-          ;
-        
-        console.log('selector:', args.selectors, 'scope: ' + scope + ' ' + scope.particulars );
-        
-        //result = selectorEngine.query(multivers, args.selectors)
-        result = scope.queryAll(args.selectors);
-        
+        var result = controller.queryAll(args.selectors);
         console.log('result:')
         console.log(result.map(function(item){ return item +' '+item.particulars }).join(',\n'));
         console.log('_______________');
