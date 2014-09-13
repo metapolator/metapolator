9981a05c2082d42490d2a52df653a11078497d4a
Jeroen Breen <jeroen@innouveau.nl>
2014-09-13 23:57:29 +0200

    model structure idea

---
+++ slider/js/structure.js
@@ -0,0 +1,183 @@
+app.controller("MetapolatorController", function($scope) {
+	$scope.project = {
+		projectName: "untitled document",
+		display: {
+			mode: ["glyphview", "paragraph", "", ""],
+			fontsize: 40,
+			lineHeight: 36			
+		},
+		parameters: [
+			{
+				type: "slant",
+				operation: "*1.1"	
+			}
+		],
+		sequences: [
+			{
+				name: "weight",
+				paramters: [],
+				masters: [
+					{
+						name: "master 1",
+						type: "master",
+						display: true,
+						parameters: [],
+						body: {
+							glyphs: [
+								{
+									name: "a",
+									parameters: [],
+									skeleton: [],
+									vector: []
+								},
+								{
+									name: "b",
+									parameters: [],
+									skeleton: [],
+									vector: []
+								},
+								{
+									name: "c",
+									parameters: [],
+									skeleton: [],
+									vector: []
+								}
+							]
+						}
+					},
+					{
+						name: "master 2",
+						type: "master",
+						display: false,
+						parameters: [],
+						body: {
+							glyphs: [
+								{
+									name: "a",
+									parameters: [],
+									skeleton: [],
+									vector: []
+								},
+								{
+									name: "b",
+									parameters: [],
+									skeleton: [],
+									vector: []
+								},
+								{
+									name: "c",
+									parameters: [],
+									skeleton: [],
+									vector: []
+								}
+							]
+						}
+					},
+			{
+				name: "width",
+				parameters: [
+					{
+						type: "Direction in",
+						operation: "=1"
+					},
+					{
+						type: "Direction in max",
+						operation: "2.0"
+					}
+				],
+				masters: [
+					{
+						name: "master 3",
+						type: "adjustment master",
+						display: false,
+						parameters: [],
+						body: {
+							glyphs: [
+								{
+									name: "a",
+									parameters: [],
+									skeleton: [],
+									vector: []
+								},
+								{
+									name: "b",
+									parameters: [],
+									skeleton: [],
+									vector: []
+								},
+								{
+									name: "c",
+									parameters: [],
+									skeleton: [],
+									vector: []
+								}
+							]
+						}
+					},
+					{
+						name: "master 2",
+						type: "master",
+						display: false,
+						parameters: [],
+						body: {
+							glyphs: [
+								{
+									name: "a",
+									parameters: [],
+									skeleton: [],
+									vector: []
+								},
+								{
+									name: "b",
+									parameters: [],
+									skeleton: [],
+									vector: []
+								},
+								{
+									name: "c",
+									parameters: [],
+									skeleton: [],
+									vector: []
+								}
+							]
+						}
+					}
+				
+				]
+			}
+		],
+		stringOfInstances: [
+			{
+				name: "string 1",
+				instances: [
+					{
+						name: "instance 1",
+						metapolation: [
+							{
+								master: $scope.project.sequences[0].master[0],
+								percentage: 4
+							},
+							{
+								master: scope.project.sequences[0].master[1],
+								percentage: 96
+							}
+						]
+					},
+					{
+						name: "instance 2",
+						metapolation: [
+							{
+								master: $scope.project.sequences[0].master[0],
+								percentage: 78
+							},
+							{
+								master: scope.project.sequences[0].master[1],
+								percentage: 22
+							}
+						]
+					}
+				]
+			}
+		]
+	}
+});
