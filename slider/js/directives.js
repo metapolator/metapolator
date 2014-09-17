24a3bc9cc841a9d229904892e33e729876a1ac59
Jeroen Breen <jeroen@innouveau.nl>
2014-09-18 00:35:19 +0200

    Sortable changes correspond with model
    by using sortable.js

--- slider/js/directives.js
+++ slider/js/directives.js
@@ -1,76 +1,58 @@
-app.directive('uiSelectable', function ($compile) {
-    return function (scope, el, attrs) {
-    	
-        el.sortable({
-        	 handle: ".handle", 
-        	 connectWith: "ul"
-        })
-        .on( "sortreceive", function( scope, el ) {
-        	// if last sequence is not empty anymore
-        	//var last = scope.masterSequences.length + 1;  	
-        	var last = 2;
-        	if ($("#master-ul-" + last + " li").length > 0) {
-        		// create new empty ul
-        		last++;
-        		console.log($(this).parent());
-        		var toAppendTo = $(this).parent();
-        		toAppendTo.append("<ul ui-selectable class='ui-selectable' id='master-ul-" + last + "'></ul>");
-        		
-		        //$compile(toAppendTo)(scope);
-		        //element.append(toAppendTo);
-        	}
-        } )
-        .selectable({
-        	cancel: ".selectable-name, .selectable-diamond, .handle", 
-            stop: function( event, ui ) {
-            	
-            	// still needs freshing up
-            	var type = el.context.id.split("-");
-             	
-                var highlighted = el.find('.ui-selected').map(function() {
-                	var found = $(this).context.id.split("-");
-                	return found[1];
-                }).get();
+/*
+app.directive('uiSelectable', function($compile) {
+    return function(scope, el, attrs) {
+
+        el.selectable({
+            cancel : ".selectable-name, .selectable-diamond, .handle",
+            stop : function(event, ui) {
                 
-							
-				var viewSet = new Array ();
-				for (var i = 0; i < highlighted.length; i++) {
-					viewSet[i] = highlighted[i];
-				}
-				if (type[0] == "adjustmentMaster") {
-					scope.adjustmentMastersInView = viewSet;
-					scope.$apply();
-				}
-				if (type[0] == "master") {
-					scope.mastersInView = viewSet;
-					scope.$apply();
-				}
+                console.log ("!");
+
+                // still needs freshing up
+                var type = el.context.id.split("-");
+
+                var highlighted = el.find('.ui-selected').map(function() {
+                    var found = $(this).context.id.split("-");
+                    return found[1];
+                }).get();
+
+                var viewSet = new Array();
+                for (var i = 0; i < highlighted.length; i++) {
+                    viewSet[i] = highlighted[i];
+                }
+                if (type[0] == "adjustmentMaster") {
+                    scope.adjustmentMastersInView = viewSet;
+                    scope.$apply();
+                }
+                if (type[0] == "master") {
+                    scope.mastersInView = viewSet;
+                    scope.$apply();
+                }
 
             }
         })
     };
 });
+*/
 
 
 app.directive('dividerHor', function() {
-	return {
-	    template: '<div class="divider-hor"><div class="divider-top"></div><div class="divider-bottom"></div>',
-	    restrict: 'E',
-	    transclude: true,
-	    replace: true,
-	    require: 'ngModel'
-	};
-}); 
+    return {
+        template : '<div class="divider-hor"><div class="divider-top"></div><div class="divider-bottom"></div>',
+        restrict : 'E',
+        transclude : true,
+        replace : true,
+        require : 'ngModel'
+    };
+});
 
 app.directive('dividerVer', function() {
-	return {
-	    template: '<div class="divider-ver"><div class="divider-left"></div><div class="divider-right"></div>',
-	    restrict: 'E',
-	    transclude: true,
-	    replace: true,
-	    require: 'ngModel'
-	};
-}); 
-
-
+    return {
+        template : '<div class="divider-ver"><div class="divider-left"></div><div class="divider-right"></div>',
+        restrict : 'E',
+        transclude : true,
+        replace : true,
+        require : 'ngModel'
+    };
+});
 
--- slider/js/maincontroller.js
+++ slider/js/maincontroller.js
@@ -1,109 +1,112 @@
-app.controller("MetapolatorController", function($scope){
-	
-	$scope.specimen = {
-		text: "Freude, schöner Götterfunken, Tochter aus Elysium",
-		size: 100
-	};
-	
-	$scope.masters = [
-        {
-        	id: 0,
-            fontFamily: 'Lato',
-            name: '',
-            weight: '100'
-        },
-        {
-        	id: 1,
-            fontFamily: 'Lato',
-            name: '',
-            weight: '300'
-        },
-        {
-        	id: 2,
-            fontFamily: 'Lato',
-            name: '',
-            weight: '400'
-        },
-        {
-        	id: 3,
-            fontFamily: 'Lato',
-            name: '',
-            weight: '700'
-        }
-	];
-	
-	$scope.masterSequences = [
-		[0, 1, 2, 3]
-	
-	];
-	
-	$scope.adjustmentMasters = [
-		{
-        	id: 0,
-            fontFamily: 'Merriweather',
-            name: 'adj. 1',
-            weight: '300'
-		},
-		{
-        	id: 1,
-            fontFamily: 'Merriweather',
-            name: 'adj. 2',
-            weight: '700'
-		}
-	];
-	
-	$scope.instances = [
-		{
-            fontFamily: 'Droid sans mono',
-            name: '',
-            weight: '400'
-		},
-		{
-            fontFamily: 'Lato',
-            name: '',
-            weight: '900'
-		}
-	];
-	
-	
-	$scope.mastersInView = [];
-	$scope.adjustmentMastersInView = [];
-	
-    $scope.addMaster = function () {
+app.controller("MetapolatorController", function($scope) {'use strict';
+
+    $scope.specimen = {
+        text : "Freude, schöner Götterfunken, Tochter aus Elysium",
+        fontSize : 60,
+        lineHeight: 60
+    };
+
+    $scope.sequences = [{
+        name : "Weight",
+        sortOrder : 0,
+        masters : [{
+            fontFamily : 'Roboto',
+            name : 'we-Light',
+            weight : '100',
+            display : true
+        }, {
+            fontFamily : 'Roboto',
+            name : 'we-Regular',
+            weight : '400',
+            display : true
+        }, {
+            fontFamily : 'Roboto',
+            name : 'we-Bold',
+            weight : '700',
+            display : true
+        }]
+    }, {
+        name : "Width",
+        sortOrder : 1,
+        masters : [{
+            fontFamily : 'Roboto Condensed',
+            name : 'w-Regular',
+            weight : '400',
+            display : true
+        }, {
+            fontFamily : 'Roboto Condensed',
+            name : 'w-Bold',
+            weight : '700',
+            display : true
+        }]
+    }, {
+        name : "Slab",
+        sortOrder : 2,
+        masters : [{
+            fontFamily : 'Roboto Slab',
+            name : 's-Regular',
+            weight : '400',
+            display : false
+        }, {
+            fontFamily : 'Roboto Slab',
+            name : 's-Bold',
+            weight : '700',
+            display : true
+        }]
+    }];
+
+    $scope.adjustmentMasters = [{
+        fontFamily : 'Droid sans mono',
+        name : '',
+        weight : '400'
+    }, {
+        fontFamily : 'Lato',
+        name : '',
+        weight : '900'
+    }];
+
+    $scope.mastersInView = [];
+    $scope.adjustmentMastersInView = [];
+
+    $scope.addMaster = function() {
         if (!$scope.newMaster.fontFamily.length) {
             return;
         }
         $scope.masters.push({
-            fontFamily: $scope.newMaster.fontFamily,
-            name: $scope.newMaster.name,
-            weight: $scope.newMaster.weight
+            fontFamily : $scope.newMaster.fontFamily,
+            name : $scope.newMaster.name,
+            weight : $scope.newMaster.weight
         });
 
         $scope.newMaster = '';
     };
     $scope.newMaster = {
-    	fontFamily: '',
-    	name: '',
-    	weight: ''	
+        fontFamily : '',
+        name : '',
+        weight : ''
     };
 
-	
-    $scope.addAdjustmentMaster = function () {
+    $scope.addAdjustmentMaster = function() {
         if (!$scope.newAdjustmentMaster.fontFamily.length) {
             return;
         }
         $scope.adjustmentMasters.push({
-            fontFamily: $scope.newAdjustmentMaster.fontFamily,
-            name: $scope.newAdjustmentMaster.name,
-            weight: $scope.newAdjustmentMaster.weight
+            fontFamily : $scope.newAdjustmentMaster.fontFamily,
+            name : $scope.newAdjustmentMaster.name,
+            weight : $scope.newAdjustmentMaster.weight
         });
 
         $scope.newAdjustmentMaster = '';
     };
     $scope.newAdjustmentMaster = {
-    	fontFamily: '',
-    	name: '',
-    	weight: ''	
+        fontFamily : '',
+        name : '',
+        weight : ''
+    };
+
+    $scope.sortableOptions = {
+        connectWith : ".master-ul",
+        cancel : ".selectable-ag"
     };
 
 });
