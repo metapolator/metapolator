2232872b8028856a1a6b0ed4326f82fdc2dccc13
Jeroen Breen <jeroen@innouveau.nl>
2014-09-19 12:05:55 +0200

    Virtual dividers
    instead of old plan with 6 real dividers, now almost everything working
    with 2 virtual dividers.
    Dividers influence there parent width (#landscap), thereby their
    position gets disturbed, uncontrollable, because of the ‘helper’
    position is quite uncontrollable.
    Virtual dividers are completely independent of #landscape, which makes
    them far more controllable.

--- slider/js/functions.js
+++ slider/js/functions.js
@@ -1,44 +1,24 @@
 $(function() {
-    //updateContainment();
-
+    updateContainment();
     function updateContainment() {
-        var containment_1 = parseInt($("#border-2").offset().left) - 150;
-        var containment_2 = parseInt($("#border-1").offset().left) + 150;
-        var containment_3 = parseInt($(window).outerWidth()) - 150;
-        var containment_4 = parseInt($(window).outerWidth()) + 150;
-        var containment_5 = parseInt($("#border-4").offset().left) - 150;
-        var containment_6 = parseInt($("#border-3").offset().left) + 150;
-        var containment_7 = 2 * parseInt($(window).outerWidth()) - 150;
-        var containment_8 = 2 * parseInt($(window).outerWidth()) + 150;
-        var containment_9 = parseInt($("#border-6").offset().left) - 150;
-        var containment_10 = parseInt($("#border-5").offset().left) + 150;
-        var containment_11 = 3 * parseInt($(window).outerWidth()) - 150;
+        var containment_1 = parseInt($("#divider-2").offset().left) - 150;
+        var containment_2 = parseInt($("#divider-1").offset().left) + 150;
+        var containment_3 = $(window).outerWidth() - 150;
 
-        $("#border-1").draggable({
+        $("#divider-1").draggable({
             containment : [150, 0, containment_1, 0]
         });
-        $("#border-2").draggable({
+        $("#divider-2").draggable({
             containment : [containment_2, 0, containment_3, 0]
-        });
-        $("#border-3").draggable({
-            containment : [containment_4, 0, containment_5, 0]
-        });
-        $("#border-4").draggable({
-            containment : [containment_6, 0, containment_7, 0]
-        });
-        $("#border-5").draggable({
-            containment : [containment_8, 0, containment_9, 0]
-        });
-        $("#border-6").draggable({
-            containment : [containment_10, 0, containment_11, 0]
         });
     }
 
     var id, leftPanel, rightPanel, leftStart, rightStart, mirror = 0;
-    $(".border").draggable({
+
+    $(".divider").draggable({
         axis : "x",
         start : function(event, ui) {
-            id = parseInt($(this).context.id.split("-")[1]);
+            id = parseInt($(this).context.id.split("-")[1]) + 2 * currentView;
             leftPanel = $("#panel-" + id);
             rightPanel = $("#panel-" + (id + 1));
             leftStart = leftPanel.outerWidth();
@@ -59,31 +39,22 @@ $(function() {
 
         },
         drag : function(event, ui) {
-            //console.log(ui.originalPosition.left + " - " + ui.position.left + " - " + ui.offset.left);
             leftPanel.css("width", leftStart + (ui.position.left - ui.originalPosition.left));
             rightPanel.css("width", rightStart - (ui.position.left - ui.originalPosition.left));
+
             if (mirror != 0) {
                 if (id % 2 == 0) {
                     $("#panel-" + mirror).css("width", mirrorStart + (ui.position.left - ui.originalPosition.left));
                 } else {
                     $("#panel-" + mirror).css("width", mirrorStart - (ui.position.left - ui.originalPosition.left));
                 }
-                $("#border-" + mirror).css("left", $("#panel-" + (mirror + 1)).css("left"));
             }
-            var a = parseInt($("#panel-1").outerWidth());
-            var b = parseInt($("#panel-2").outerWidth());
-            var c = parseInt($("#panel-3").outerWidth());
-            var d = parseInt($("#panel-4").outerWidth());
-            var e = parseInt($("#panel-5").outerWidth());
-            var f = parseInt($("#panel-6").outerWidth());
-            var g = parseInt($("#panel-7").outerWidth());
-
-            console.log(a + " + " + b + " + " + c + " = " + (a + b + c) + " / " + c + " + " + d + " + " + e + " = " + (c + d + e) + " / " + e + " + " + f + " + " + g + " = " + (e + f + g));
+            moveLandscape(currentView, 0);
 
         },
         stop : function(event, ui) {
             mirror = 0;
-            //updateContainment();
+            updateContainment();
         }
     });
 
@@ -92,10 +63,24 @@ $(function() {
     });
 });
 
+var currentView = 0;
 
-function moveLandscape(view) {
+function moveLandscape(view, transition) {
+    // move virtual dividers
+    currentView = view;
+    $("#divider-1").css("left", $("#panel-" + (view * 2 + 1)).outerWidth());
+    $("#divider-2").css("left", $("#panel-" + (view * 2 + 1)).outerWidth() + $("#panel-" + (view * 2 + 2)).outerWidth());
+
+    // move landscape
+    if (transition == 1) {
+        $("#landscape").css("transition",  "all 700ms ease");
+    } else {
+        $("#landscape").css("transition",  "none");
+    }
     var thisLeft = -$("#panel-" + (view * 2 + 1)).position().left;
     $("#landscape").css("left", thisLeft);
+
+    // set menu buttons
     for (var i = 0; i < 3; i++) {
         $("#menu-item-" + i).removeClass("menu-item-current");
     }
