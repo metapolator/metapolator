41a85e0d8f64a0ac8d280896704017b57fbd18ca
Reuben Thomas <rrt@sc3d.org>
2014-09-21 23:15:49 +0100

    Overhaul StyleDict.get
    Merge _get into get and rewrite the documentation.

--- app/lib/models/CPS/StyleDict.js
+++ app/lib/models/CPS/StyleDict.js
@@ -38,8 +38,8 @@ define([
     _p._getCPSParameterValue = function(name) {
         var i=0, value;
         if(!this._controller.parameterRegistry.exists(name))
-            throw new KeyError('Name "'+ name +'" is no known parameter, '
-                +'it must be registered before you can use it.');
+            throw new KeyError('No such parameter "'+ name +'" '
+                +'has been registered.');
         for(;i<this._rules.length;i++) {
             try {
                 value = this._rules[i].parameters.get(name);
@@ -76,101 +76,64 @@ define([
      * Return the value of a ParameterValue instance.
      * Raises KeyError and CPSKeyNotFoundError, see this._getParameter
      * for a description.
-     *
-     * This used to cache the result of this._getParameter but we decided
-     * to drop that behavior. Now the instance of the ParameterValue
-     * is rebuild every time (currently either a CPSVector or a CPSReal)
      */
     _p.getParameter = function(name) {
         return this._getParameter(name).getValue();
     };
 
     /**
-     * Return a value for this._element if name is a registered parameter
-     * type the return value type is checked according to the parameter
-     * type.
+     * Look up a parameter in this._element according to the following
+     * rules:
      *
-     * ALso, as an exception: "this" will always return the MOM Element
-     * of this StyleDict (return this._element).
+     * 1. If `name' is "this", return the MOM Element of this StyleDict
+     * (this._element). We check "this" first so it can't be overridden by
+     * a @dictionary rule. (It would also be possible to define this as a
+     * CPSParameter, so we could get it via this.getParameter.)
      *
-     * If name is not a registered parameter type the @dictionary rules for
-     * this element is are queried.
+     * 2. If `name' is a registered parameter name, look it up. If this
+     * fails, throw CPSKeyNotFoundError, so the caller knows to check the
+     * next parameter value source.
      *
-     * If name is not an @dictionary parameter, it is queried on element
-     * itself.
+     * 3. Look up `name' in the @dictionary rules for this element.
      *
-     * If name is not a registered parameter type, the return value may be
-     * anything that is accessible or constructable from CPSformulae and
-     * also a white-listed value on any reachable element.
+     * 4. Look up `name' in the element itself. If this fails, throw
+     * KeyError.
      *
-     * This API tries three sources of data:
-     *    if name is a registered Parameter name
-     *          this.getParameter(name); // raises CPSKeyNotFoundError if not defined
-     *    else
-     *          1. ReferenceDictionary: this._controller
-     *                                .getReferenceDictionary(this._element)
-     *                                .get(name);
-     *          2. this._element[name] // needs proper whitelisting
-     *
-     * If name is a registered parameter name and it is not found
-     * CPSKeyNotFoundError is thrown.
-     * If name is not a registered parameter and it is not found,
-     * KeyError is thrown.
-     *
-     * We can use the CPSKeyNotFoundError to search for the next parameter
-     * value source and the KeyError to inform the user that an unknown name
-     * was used.
+     * If `name' is a registered parameter type, the return value's type is
+     * the parameter type; otherwise, the return value may be anything that
+     * is accessible or constructable from CPSformulae, or a white-listed
+     * value on any reachable element.
      */
-    _p._get = function(name) {
-        // "this" is not defined for cpsGetters.whiteList,
-        // because I want "this" to be not overridable by @dictionary.
-        // thus it's queried first. Also it would be possible to define
-        // this as a CPSParameter, so we could get it via this.getParameter.
+    _p.get = function(name) {
         if(name === 'this')
             return this._element;
 
-        if(this._controller.parameterRegistry.exists(name))
-            // The name is a registered parameter, so we always know
-            // its type and purpose.
-            // This raises CPSKeyNotFoundError to indicate that the next
-            // layer of a parameter source may take over.
-            // Default values are not part of the parameter definition
-            // anymore we'll have to define our default values with CPS if
-            // we need any.
-            return this.getParameter(name);
-
-        // @dictionary values can be used to override in-element-lookups
-        // get the ReferenceDictionary for element
-        try {
-            return this._controller
-                       .getReferenceDictionary(this._element)
-                       .get(name);
-        }
-        catch(error) {
-            if(!(error instanceof KeyError))
-                throw error;
-        }
-        // Will throw KeyError if not found
-        return cpsGetters.whitelist(this._element, name);
-    };
-
-    _p.get = function(name) {
-        var error, result;
+        // Detect recursion on this._element
         if(name in this._getting)
             throw new CPSRecursionError('Looking up "' + name
                             + '" is causing recursion in the element: '
                             + this._element.particulars);
-        this._getting[name] = null;
+        this._getting[name] = true;
         try {
-            result = this._get(name);
+            if(this._controller.parameterRegistry.exists(name))
+                // Will throw CPSKeyNotFoundError if not found.
+                return this.getParameter(name);
+
+            try {
+                return this._controller
+                       .getReferenceDictionary(this._element)
+                       .get(name);
+            }
+            catch(error) {
+                if(!(error instanceof KeyError))
+                    throw error;
+            }
+            // Will throw KeyError if not found
+            return cpsGetters.whitelist(this._element, name);
         }
-        catch(err) {
-            error = err
+        finally {
+            delete this._getting[name];
         }
-        delete this._getting[name];
-        if(error)
-            throw error
-        return result;
     }
 
     return StyleDict;
