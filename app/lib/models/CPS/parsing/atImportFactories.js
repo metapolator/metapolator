154392cb507a3072b237104c06e0f190de3d7191
Lasse Fister <commander@graphicore.de>
2014-11-19 03:46:27 +0100

    new module CPS/elements/AtImportCollection

--- app/lib/models/CPS/parsing/atImportFactories.js
+++ app/lib/models/CPS/parsing/atImportFactories.js
@@ -2,16 +2,14 @@ define([
     'metapolator/errors'
   , './curry'
   , './parameterFactories'
-  , 'metapolator/models/CPS/elements/Parameter'
-  , 'metapolator/models/CPS/elements/AtRuleCollection'
+  , 'metapolator/models/CPS/elements/AtImportCollection'
   , 'metapolator/models/CPS/elements/AtRuleName'
   , 'metapolator/models/CPS/elements/GenericCPSNode'
 ], function (
     errors
   , curry
   , parameterFactories
-  , Parameter
-  , AtRuleCollection
+  , AtImportCollection
   , AtRuleName
   , GenericCPSNode
 ) {
@@ -19,7 +17,7 @@ define([
     var CPSError = errors.CPS
       , genericNameFactory = parameterFactories.genericNameFactory
       ;
-      
+
     /**
      * override constructors for the purpose of @import.
      * This means for all children of @import we can define other rules.
@@ -40,41 +38,23 @@ define([
          * Find the name of the resource to load and return a ParameterCollection
          */
         'atrules': function(node, source, ruleController) {
-            var args, resource;
+            var args, resource, parameterCollection;
             // filter all whitespace
             args = node.children.slice(1).filter(function(child) {
                 if(child.instance instanceof GenericCPSNode && child.instance.type === 's')
                     return false;
                 return true;
             });
-            
+
             // accept only one argument which must be a string
             if(args.length !== 1
                 || !(args[0].instance instanceof GenericCPSNode
                                     && args[0].instance.type === 'string'))
                 return this,['__GenericAST__'](node, source);
             resource = args[0].data.slice(1,-1);
-            
-            // TODO:
-            // * It is desired to create and return a special
-            //   subclass of ParameterCollection => AtImportCollection,
-            //   This will be a good marker for us and allow for all kinds
-            //   of specialization!
-            //   This could be done by using a (sub-)version of parameterFactories
-            //   that overides the "stylesheet" factory, which is declared in
-            //   baseFactories.
-            // * The new subclass of ParameterCollection will stringify as the
-            //   original @import directive
-            // * This change breaks the red pill on a deep level:
-            //   changing one of these rules should propagate to all instances
-            //   where this file was imported.
-            // * ALSO: When a change to this file
-            //   happens, a reloading of the affected styles MUST NOT read
-            //   this file's version from disc, but use the buffered version
-            //   as a source.
-            
-            //parse the file and return the resulting ParameterCollection
-            return ruleController.getRule(false, resource);
+
+            parameterCollection = ruleController.getRule(false, resource)
+            return new AtImportCollection(resource, parameterCollection);
         }
       , 'atkeyword': curry(genericNameFactory, AtRuleName)
     });
