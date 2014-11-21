46182982502a4995926aadf29901f5336dda748e
Lasse Fister <commander@graphicore.de>
2014-11-22 00:06:22 +0100

    Fix bug: RuleControler.prototype.reloadRules getting stuck after an error.
    Before requesting new rules, we deleted the old rules
    to clear the cache. This lead to a moment where requests
    to `reloadRule` where answered with a KeyError, because
    the rule was pruned. When reloading  of a rule failed, because
    of a syntax bug in the cps for example, the reloadRule method
    was blocked forever.

--- app/lib/models/CPS/RuleController.js
+++ app/lib/models/CPS/RuleController.js
@@ -34,7 +34,6 @@ define([
         this._commissionIdCounter = 0;
         this._rules = Object.create(null);
         this._importing = {};
-        this._references = Object.create(null);
     }
     var _p = RuleController.prototype;
 
@@ -43,6 +42,22 @@ define([
             return this._parameterRegistry;
         }
     });
+
+    _p._isCached = function(sourceName) {
+        return (sourceName in this._rules) && this._rules[sourceName].cached;
+    };
+
+    _p._set = function(sourceName, rule, commissionId) {
+        var record;
+        if(!(sourceName in this._rules))
+            record = this._rules[sourceName] = {parameterCollection: rule};
+        else {
+            record = this._rules[sourceName];
+            record.parameterCollection.reset(rule.items, rule.source, rule.lineNo);
+        }
+        record.commissionId = commissionId;
+        record.cached = true;
+    };
 
     _p._readFile = function(async, fileName) {
                             return this._io.readFile(async, fileName); };
@@ -58,24 +73,19 @@ define([
           , cps: [false, 'fileName', 'commissionId', _p._readFile]
           , rule: ['cps', 'sourceName', 'commissionId' ,
                 function(cps, sourceName, bypassCache, commissionId) {
-                    if(!(sourceName in this._rules)
+                    if(!this._isCached(sourceName)
                             // There is a current cache but it was commissioned
                             // before this request, and finished loading before it.
                             // FIXME: a maybe better alternative would be
                             //        to fail here!
-                            || (sourceName in this._rules) && commissionId >= this._rules[sourceName][0])
+                            || this._isCached(sourceName) && commissionId >= this._rules[sourceName].commissionId)
                     {
                         var rule = parseRules.fromString(cps, sourceName, this);
-                        if(!(sourceName in this._references))
-                            this._references[sourceName] = rule;
-                        else
-                            this._references[sourceName].reset(rule.items, rule.source, rule.lineNo);
-                        this._rules[sourceName] = [commissionId, this._references[sourceName]];
+                        this._set(sourceName, rule, commissionId);
                     }
-                    return this._rules[sourceName][1];
+                    return this._rules[sourceName].parameterCollection;
                 }]
-          , isCached: ['sourceName', function(sourceName) {
-                return sourceName in this._rules;}]
+          , isCached: ['sourceName', _p._isCached]
           , commissionId:[function(){ return this._commissionIdCounter++;}]
         }
       , {cps: [true, 'fileName', 'commissionId', _p._readFile]}
@@ -83,7 +93,7 @@ define([
       , function(obtain, sourceName) {
             if(!obtain('isCached'))
                 return obtain('rule');
-            return this._rules[sourceName][1];
+            return this._rules[sourceName].parameterCollection;
         }
     );
 
@@ -94,7 +104,8 @@ define([
         if(!(sourceName in this._rules))
             throw new KeyError('Can\'t reload rule "'+ sourceName
                                 +'" because it\'s not in this controller');
-        delete this._rules[sourceName];
+        // mark as uncached
+        this._rules[sourceName].cached = false;
         return this.getRule(async, sourceName);
     };
     return RuleController;
