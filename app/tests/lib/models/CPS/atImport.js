f603f585b8265db56fb525d17db732216382d22b
Lasse Fister <commander@graphicore.de>
2014-11-22 03:51:48 +0100

    Fix: false positives in atImport recursion detection
    Add test: false positive in atImport recursion detection
    FIX: a previous merge removed the invalidation of the recursion detection

--- app/tests/lib/models/CPS/atImport.js
+++ app/tests/lib/models/CPS/atImport.js
@@ -39,5 +39,20 @@
               , CPSRecursionError
             );
         }
+       , CPS_atImport_detect_recursion_false_positives: function() {
+            var io = new TestingIO()
+              , ruleController = new RuleController(io, parameterRegistry, '')
+              ;
+            io.writeFile(false, '/a', '/* not recursive CPS */');
+            // The second call fails with CPSRecursionError, but it shouldn't,
+            // because this is not recursion, but paralell.
+            // The error is: 'CPSRecursionError: a @imports itself'
+            // This is not correct and not desired. Instead the promise
+            // should not fail.
+            // A failing promise itself marks a failing test, this metric
+            // should be enough in this case.
+            ruleController.getRule(true, 'a');
+            return ruleController.getRule(true, 'a');
+        }
     });
 });
