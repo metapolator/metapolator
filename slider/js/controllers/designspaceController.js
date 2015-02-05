40a75c6269639b84b2b4356bf86f703a8ba87d86
Jeroen Breen <jeroen@innouveau.nl>
2015-02-05 14:58:01 +0100

    added zero rule on control sliders

--- slider/js/controllers/designspaceController.js
+++ slider/js/controllers/designspaceController.js
@@ -15,28 +15,61 @@ app.controller('designspaceController', function($scope, $http, sharedScope) {
         });
         $scope.data.currentDesignSpace = i;
     }
-    
+
     $scope.output = [];
     $scope.total = 0;
-    
-    $scope.getMetapolationRatios = function (data) {
+
+    $scope.getMetapolationRatios = function(data) {
         $scope.output = [];
         var axes = data.axes;
         var n = axes.length;
+        var foundZero = false;
         var cake = 1;
         for (var i = 0; i < n; i++) {
-            cake += axes[i].value / ( 100 - axes[i].value);
+            if (axes[i].value == 100) {
+                foundZero = true;
+                metapolationRatiosWithZero(data);
+                break;
+            }
+            cake += axes[i].value / (100 - axes[i].value);
+
         }
-        $scope.output.push("Master 0: " + roundup(1 / cake));
-        for (var i = 0; i < n; i++) {
-            var piece = axes[i].value / ( 100 - axes[i].value);
-            $scope.output.push("Master " + (i + 1) + ": " + roundup(piece/cake));
+        if (!foundZero) {
+            $scope.output.push("Master 0: " + roundup(1 / cake));
+            for (var i = 0; i < n; i++) {
+                var piece = axes[i].value / (100 - axes[i].value);
+                $scope.output.push("Master " + (i + 1) + ": " + roundup(piece / cake));
+            }
         }
     };
-    
-    function roundup (a) {
+
+    function metapolationRatiosWithZero(data) {
+        $scope.output = [];
+        var axes = data.axes;
+        var n = axes.length;
+        var cake = 0;
+        var mastersNotZero = [];
+        $scope.output.push("Master 0: " + 0);
+        for (var i = 0; i < n; i++) {
+            if (axes[i].value == 100) {
+                mastersNotZero.push(1);
+                cake++;
+            } else {
+                mastersNotZero.push(0);
+            }
+        }
+        for (var i = 0; i < n; i++) {
+            if (mastersNotZero[i] == 1) {
+                $scope.output.push("Master " + (i + 1) + ": " + roundup(1 / cake));
+            } else {
+                $scope.output.push("Master " + (i + 1) + ": 0");
+            }
+        }
+    }
+
+    function roundup(a) {
         var b = Math.round(a * 1000) / 1000;
         return b;
     }
-    
+
 });
