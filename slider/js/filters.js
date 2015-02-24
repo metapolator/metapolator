fe7dad7e810cac35dbc620d6e662051ecaa4b20c
Jeroen Breen <jeroen@innouveau.nl>
2015-02-24 22:52:01 +0100

    strict functionality

--- slider/js/filters.js
+++ slider/js/filters.js
@@ -6,14 +6,14 @@ app.filter('rangeFilter', function() {
             if (filter.length == 0) {
                 filtered.push(thisGlyph);
             } else {
-                if(filter.indexOf(thisGlyph.value) > -1) {
+                if (filter.indexOf(thisGlyph.value) > -1) {
                     filtered.push(thisGlyph);
-                } 
+                }
             }
         }
         return filtered;
     };
-}); 
+});
 
 app.filter('specimenFilter', function() {
     return function(specimen, options, sequences) {
@@ -25,27 +25,32 @@ app.filter('specimenFilter', function() {
             var newText = "";
             var text = string.split(" ");
             var filter = options.filter;
-    
-            if (filter.length == 0) {
-                newText = string;
+
+            if (strict == 3) {
+                newText = filter;
             } else {
-                text.forEach(function(word) {
-                    var hits = 0;
-                    for (var i = 0; i < word.length; i++) {
-                        if (filter.indexOf(word[i]) > -1) {
-                            hits++;
+
+                if (filter.length == 0) {
+                    newText = string;
+                } else {
+                    text.forEach(function(word) {
+                        var hits = 0;
+                        for (var i = 0; i < word.length; i++) {
+                            if (filter.indexOf(word[i]) > -1) {
+                                hits++;
+                            }
+                            if (strict == 3) {
+                                required = word.length;
+                            }
+                            if (hits >= required) {
+                                newText += word + " ";
+                                break;
+                            }
                         }
-                        if (strict == 3) {
-                            required = word.length;
-                        }
-                        if (hits >= required) {
-                            newText += word + " ";
-                            break;
-                        }
-                    }
-                });
+                    });
+                }
             }
-    
+
             // find the masters display true
             var masterArray = [];
             for (var j = 0; j < sequences.length; j++) {
@@ -53,25 +58,23 @@ app.filter('specimenFilter', function() {
                     var thisFont = sequences[j].masters[k];
                     if (thisFont.display == true) {
                         var foundFont = {
-                            "sequenceId": j,
-                            "masterId": k,
-                            "family": thisFont.fontFamily,
-                            "weight": thisFont.weight,
-                            "name": thisFont.name
+                            "sequenceId" : j,
+                            "masterId" : k,
+                            "family" : thisFont.fontFamily,
+                            "weight" : thisFont.weight,
+                            "name" : thisFont.name
                         };
                         masterArray.push(foundFont);
                     }
                 }
             }
-            
-           
-    
+
             var filtered = [];
             var masterIndex = 0;
-            
+
             for (var i = 0; i < newText.length; i++) {
                 var glyph = newText[i];
-                
+
                 // adding linebreak property for paragraphs
                 var linebreak = false;
                 if (glyph == "<" && newText[i + 1] == "b" && newText[i + 2] == "r") {
@@ -81,8 +84,8 @@ app.filter('specimenFilter', function() {
                 }
                 var master = masterArray[masterIndex];
                 var thisElement = {
-                    "glyph": glyph, 
-                    "master": master,
+                    "glyph" : glyph,
+                    "master" : master,
                     "linebreak" : linebreak
                 };
                 filtered.push(thisElement);
@@ -94,8 +97,8 @@ app.filter('specimenFilter', function() {
                     masterIndex = 0;
                 }
             }
-           
+
             return filtered;
         }
     };
-}); 
+});
