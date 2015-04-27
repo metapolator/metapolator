2748fa669dd24ee9dfdae7139a5fb156c3434d0f
Jeroen Breen <jeroen@innouveau.nl>
2015-04-27 23:27:38 +0200

    #465 same for specimen2 and some more specimen samples

--- purple-pill/js/controllers/specimenController.js
+++ purple-pill/js/controllers/specimenController.js
@@ -19,7 +19,7 @@ function($scope, $sce, sharedScope) {
         name : "Metapolator",
         text : "Metapolator"
     }, {
-        name : "BYO Specimen",
+        name : "[Enter your own text]",
         text : ""
     },{
         name : "Pangram 1",
@@ -33,6 +33,12 @@ function($scope, $sce, sharedScope) {
     }, {
         name : "Something with breaks",
         text : "Hey you,*nthe rock*nsteady crew."
+    }, {
+        name : "AaBbCcDdEe...",
+        text : "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789"
+    }, {
+        name : "Numbers",
+        text : "0123456789"
     }, {
         name : "Paragraph 1",
         text : "Grumpy wizards make toxic brew for the evil Queen and Jack. One morning when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.*pHe lay on his armourlike back and if he lifted his head a little, he could see his brown belly slightly domed and divided by arches into stiff sections.*pThe bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs pitifully thin compared with the size of the rest of him, waved about helplessly as he looked."
