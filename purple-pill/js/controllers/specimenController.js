63e158d062ca3e843b5ad7f56406fcbb84e44952
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2015-04-28 00:20:23 +0200

    Restore break.

--- purple-pill/js/controllers/specimenController.js
+++ purple-pill/js/controllers/specimenController.js
@@ -29,8 +29,7 @@ function($scope, $sce, sharedScope) {
         text : "Quick wafting zephyrs vex bold Jim."
     }, {
         name : "Something With Breaks",
-        text : "Hey you,*nthe rock*nsteady " +
-        "crew."
+        text : "Hey you,*nthe rock*nsteady crew."
     }, {
         name : "Paragraph 1",
         text : "Grumpy wizards make toxic brew for the evil Queen and Jack. One morning when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.*pHe lay on his armourlike back and if he lifted his head a little, he could see his brown belly slightly domed and divided by arches into stiff sections.*pThe bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs pitifully thin compared with the size of the rest of him, waved about helplessly as he looked."
