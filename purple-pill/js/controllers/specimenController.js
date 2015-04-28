ef2a8387e880e2ec109877d77dda6ed795b40db3
Jeroen Breen <jeroen@innouveau.nl>
2015-04-28 15:15:35 +0200

    #465 glyph range added as array

--- purple-pill/js/controllers/specimenController.js
+++ purple-pill/js/controllers/specimenController.js
@@ -39,13 +39,14 @@ function($scope, $sce, sharedScope) {
     }], [{
         name : "Paragraph 1",
         text : "Grumpy wizards make toxic brew for the evil Queen and Jack. One morning when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.*pHe lay on his armourlike back and if he lifted his head a little, he could see his brown belly slightly domed and divided by arches into stiff sections.*pThe bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs pitifully thin compared with the size of the rest of him, waved about helplessly as he looked."
-    }], []];
+    }]];
 
     // only for the masters specimen panel
     $scope.addGlyphRange = function() {
-        $scope.specimen[$scope.specimen.length - 1].push({
+        var glyphRange = [{
             name : "Glyph Range"
-        });
+        }];
+        $scope.specimen.push(glyphRange);
     };
 
     $scope.selectedSpecimen = $scope.specimen[0][0];
