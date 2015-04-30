78cfb22661563c2fa65b9f0c9e12f599e6d4a989
Jeroen Breen <jeroen@innouveau.nl>
2015-04-30 22:52:32 +0200

    #517 Peters specimen set

--- purple-pill/js/controllers/specimenController.js
+++ purple-pill/js/controllers/specimenController.js
@@ -21,28 +21,22 @@ function($scope, $sce, sharedScope, $http) {
         name : "[Enter your own text]",
         text : "Metapolator"
     }], [{
-        name : "Pangram 1",
-        text : "The quick brown fox jumps over the lazy dog."
+        name : "3 Pangrams",
+        text : "Quick wafting zephyrs vex bold Jim. The quick brown fox jumps over the lazy dog. Bright vixens jump dozy fowl quack."
     }, {
-        name : "Pangram 2",
-        text : "Bright vixens jump dozy fowl quack."
+        name : "Capitalised Pan",
+        text : "Aladine Biopsia Cumbia Diego Espejo Flecha Gaveta Hockey Index Jaque Kurdos Ludwing Motivo Nylon Ortiz Profit Quiff Roving Sioux Tizzy Unwary Vertex Wrathy Xammar Yachts Zaque"
     }, {
-        name : "Pangram 3",
-        text : "Quick wafting zephyrs vex bold Jim."
+        name : "Ruder",
+        text : "vertrag crainte screw, bibel malhabile modo. verwalter croyant science, biegen peuple punibile. verzicht fratricide sketchy, blind qualifier quindi. vorrede frivolité story, damals quelle dinamica. yankee instruction take, china quelque analiso. zwetschge lyre treaty, schaden salomon macchina. zypresse navette tricycle, schein sellier secondo. fraktur nocturne typograph, lager sommier singolo. kraft pervertir vanity, legion unique possibile. raffeln presto victory, mime unanime unico. reaktion prévoyant vivacity, mohn usuel legge. rekord priorité wayward, nagel abonner unione. revolte proscrire efficiency, puder agir punizione. tritt raviver without, quälen aiglon dunque. trotzkopf tactilité through, huldigen allégir quando. tyrann arrêt known, geduld alliance uomini."
     }], [{
-        name : "Something With Breaks",
-        text : "Hey you,*nthe rock*nsteady crew."
+        name : "Aa12%@…",
+        text : "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz*n1234567890*n( { [ . , ¡ ! ¿ ? * ' ‘ ’ \" \“ \” ] } ) $ € £ % @ & ¶ § ¢ † ‡"
     }, {
-        name : "AaBbCcDdEe...",
-        text : "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789"
-    }, {
-        name : "Numbers",
-        text : "0123456789"
-    }, {
-        name : "???",
-        text : "ÁæÆ≈∼@"
+        name : "Number Grid",
+        text : "12345678901*n23456789012*n34567890123*n45678901234*n56789012345*n67890123456*n78901234567*n89012345678*n90123456789*n01234567890"
     }], [{
-        name : "Paragraph 1",
+        name : "Paragraph",
         text : "Grumpy wizards make toxic brew for the evil Queen and Jack. One morning when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.*pHe lay on his armourlike back and if he lifted his head a little, he could see his brown belly slightly domed and divided by arches into stiff sections.*pThe bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs pitifully thin compared with the size of the rest of him, waved about helplessly as he looked."
     }]];
     
