f53644fe7562f36910f971ee215699c187829a85
Dave Crossland <dave@lab6.com>
2015-05-05 16:06:42 +0100

    Add spreadsheet t shirt links

--- purple-pill/js/controllers/mastersController.js
+++ purple-pill/js/controllers/mastersController.js
@@ -85,7 +85,7 @@ app.controller("mastersController", function($scope, sharedScope) {
     };
 
     $scope.importUfo = function() {
-        var message = "Want to load your own UFO?<br><br><a href='http://tspr.ng/F96Vjtsc' target='_blank' class='newtab'>Buy a Metapolator T shirt today!</a>";
+        var message = "Want to load your own UFO?<br><br>Show us you want this by buying a T shirt:<br><ul><li><a title='Support the project and buy a T shirt (USA)' href='http://teespring.com/metapolator-beta-0-3-0' target='_blank' class='newtab'>USA</a></li><li><a title='Support the project and buy a T shirt (Worldwide)' href='http://metapolator.spreadshirt.com' target='_blank' class='newtab'>Worldwide</a></li>";
         $scope.data.dialog(message, false, "close");
         $scope.data.localmenu.masters = false;
     };
