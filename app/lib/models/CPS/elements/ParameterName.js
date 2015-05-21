4246473bd826fc99db72fc8baeed824669af7584
Lasse Fister <commander@graphicore.de>
2015-05-21 16:12:00 +0200

    Add FIXME comment: detect invalid names!

--- app/lib/models/CPS/elements/ParameterName.js
+++ app/lib/models/CPS/elements/ParameterName.js
@@ -9,6 +9,9 @@ define([
      */
     function ParameterName(name, comments ,source, lineNo) {
         Parent.call(this, name, comments ,source, lineNo);
+        // FIXME: detect invalid names!
+        // Then set and use this.invalid and this message
+        // For usage, modify Parameter to include the names status as well.
     }
     var _p = ParameterName.prototype = Object.create(Parent.prototype);
     _p.constructor = ParameterName;
