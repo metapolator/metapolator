dd9b4f955e7b29ccb657f6d65cef5bed578727bb
Jeroen Breen <jeroen@innouveau.nl>
2015-07-18 23:14:42 +0200

    removed dialog model

--- app/lib/models/metapolator/displayModel/DisplayModel.js
+++ app/lib/models/metapolator/displayModel/DisplayModel.js
@@ -1,17 +1,14 @@
 define([
     '../_BaseModel'
   , './PanelModel'
-  , './DialogModel'
 ], function(
     Parent
   , PanelModel
-  , DialogModel
 ){
     "use strict";
     function DisplayModel() {
         this.localMenu = null;
         this.panel = new PanelModel();
-        this.dialog = new DialogModel();
     }
     
     var _p = DisplayModel.prototype = Object.create(Parent.prototype);
