3fe81be5ac28f0cc20d46bf1438f89352ac384d8
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2015-09-19 21:43:45 +0200

    Fix scroll direction

--- app/lib/ui/metapolator/parameter-panel/selection/selection-parameter/mousewheel/mousewheel-directive.js
+++ app/lib/ui/metapolator/parameter-panel/selection/selection-parameter/mousewheel/mousewheel-directive.js
@@ -7,9 +7,9 @@ define([], function() {
                   // We just set a keyCode on the event
                   // So scrolling is actually arrow up and down.
                   if (event.originalEvent.detail > 0 || event.originalEvent.wheelDelta > 0) {
-                    event.keyCode = 38;
-                  } else {
                     event.keyCode = 40;
+                  } else {
+                    event.keyCode = 38;
                   }
                   scope.$eval(attr.mtkMousewheel, {$event:event});
                 });
