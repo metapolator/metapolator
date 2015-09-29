621d2ed5640e6571c70ec17b4b85cb2cb1b6ecfe
Lasse Fister <commander@graphicore.de>
2015-09-29 03:12:32 +0200

    show path/trace of a rule in styleDict interface

--- app/lib/ui/redPill/cpsPanel/elements/styleDict/styleDict-directive.js
+++ app/lib/ui/redPill/cpsPanel/elements/styleDict/styleDict-directive.js
@@ -9,16 +9,54 @@ define([
 
     function styleDictDirective() {
         function link(scope, element, attrs, controller) {
+            scope.formatTrace = function(trace) {
+                var i, item, result = [], depth = '';
+                for(i=trace.length-1;i>=0;i--) {
+                    item = trace[i];
+                    switch(item.constructor.name) {
+                        case 'ParameterCollection':
+                            // NOTE: the first of these probably did not get @imported
+                            // the others most probably got.
+                            // We should however rather add the @import items to the trace
+                            // as it is easier to extend if other sources for
+                            // ParameterCollections come into being, like @generate
+                            // ParameterCollection could be ignored.
+                            // The first parameter collection, however helps
+                            // to identify the "master cps file" that is useful
+                            // information! (and not @imported ...)
+                            result.push(depth + '@import "'+item.source.name+'";');
+                            break;
+                        case 'AtNamespaceCollection':
+                            result.push(depth + '@namespace('+ item.selectorList + ')');
+                            break;
+                        default:
+                        result.push(depth + item.constructor.name);
+                    }
+                    depth = depth + '  ';
+                }
+                return result.join('\n');
+            };
+
             scope.updateNames = function(usedNamesSet) {
-                var i,l,children = element[0].children, child;
-                for(i=0,l=children.length;i<l;i++) {
-                    child = children[i];
-                    if(child.tagName.toLowerCase() !== 'mtk-cps-rule')
-                        continue;
-                    angular.element(child)
+                var i,l,j
+                    , containers = element[0].getElementsByClassName('container')[0].children
+                    , container, child
+                    ;
+                for(i=0,l=containers.length;i<l;i++) {
+                    container = containers[i];
+                    // assume that child is rather at the end of the container
+                    // element. At the moment of writing this reflects the
+                    // DOM layout
+                    for(j=container.children.length-1;j>=0;j--) {
+                        child = container.children[j];
+                        if(child.tagName.toLowerCase() !== 'mtk-cps-rule')
+                            continue;
+                        // there's just one child
+                        angular.element(child)
                            .isolateScope()
                            .updateUsedNames(usedNamesSet);
-
+                        break;
+                    }
                 }
             };
         }
--- app/lib/ui/redPill/cpsPanel/elements/styleDict/styleDict.tpl
+++ app/lib/ui/redPill/cpsPanel/elements/styleDict/styleDict.tpl
@@ -1,6 +1,31 @@
 <div>{{ctrl.element.particulars}}</div>
-<mtk-cps-rule
-        ng-repeat="item in ctrl.items track by $index + ':' + item.nodeID"
-        item="item"
+<ol class="container">
+<li ng-repeat="item in ctrl.items track by $index + ':' + item[1].nodeID">
+    <div ng-repeat="traceItem in item[2].slice().reverse()  track by $index"
+        ng-switch="traceItem.constructor.name"
+        style="padding-left: calc({{$index}} * 1em)"
+        ><div
+            ng-switch-when="ParameterCollection"
+            style="font-family: monospace;"
+            ><span ng-if="$index != 0">@import</span>
+             <span ng-if="$index == 0">root file:</span>
+             "{{traceItem.source.name}}";</div><!--
+
+             FIXME:TODO: I think it is bad having these mtk-cps-selector-list
+             elements here to be changed because it's not obvious that the
+             namespaces (usually) contain more rules. It would be better to
+             have links here that load the cps-collection which contains
+             the namespace and scroll there to the definition of that
+             namespace.
+        --><div
+            ng-switch-when="AtNamespaceCollection">@namespace(<mtk-cps-selector-list
+            selector-list-host="traceItem"
+            ></mtk-cps-selector-list>)</div><!--
+    --></div>
+    <mtk-cps-rule
+        style="padding-left: calc({{item[2].length}} * 1em)"
+        item="item[1]"
         index="$index"
     ></mtk-cps-rule>
+</li>
+<ol>
