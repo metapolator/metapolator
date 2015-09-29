bbf7702a1d9544cb3ba43fbad1a25e974ef9d3bb
Lasse Fister <commander@graphicore.de>
2015-09-29 05:32:28 +0200

    move the newProperty flag to propertyDict (from rule)

--- app/lib/ui/redPill/cpsPanel/elements/rule/rule-controller.js
+++ app/lib/ui/redPill/cpsPanel/elements/rule/rule-controller.js
@@ -9,30 +9,11 @@ define([
     function RuleController($scope) {
         this.$scope = $scope;
         $scope.index = this.index;
-        this.initNewPropertyHandler = this.initNewProperty.bind(this);
-        this.cancelNewPropertyHandler = this.cancelNewProperty.bind(this);
-        $scope.newProperty = false;
-        $scope.$on('finalizeNewProperty', this.finalizeNewProperty.bind(this));
         this.toolClickHandler = clickHandler.bind(this, 'command');
     }
 
     RuleController.$inject = ['$scope'];
     var _p = RuleController.prototype;
-
-    _p.cancelNewProperty = function() {
-        // if a new-property is open: close it
-        this.$scope.newProperty = false;
-    };
-
-    // this is currently an alias of cancelNewProperty
-    // because the new-property controller will add the new property
-    // itself to the model
-    _p.finalizeNewProperty = _p.cancelNewProperty;
-
-    _p.initNewProperty = function() {
-        // open a new-property interface
-        this.$scope.newProperty = true;
-    };
 
     return RuleController;
 });
