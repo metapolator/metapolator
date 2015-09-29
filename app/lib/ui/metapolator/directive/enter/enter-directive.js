e25ec1f767be108b97daafb0aa2bf7b9a071c515
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2015-09-29 21:44:30 +0200

    Fix 671: On key press arrow up prevent default.

--- app/lib/ui/metapolator/directive/enter/enter-directive.js
+++ app/lib/ui/metapolator/directive/enter/enter-directive.js
@@ -3,6 +3,10 @@ define([], function() {
     function enterDirective() {
         return function (scope, element, attrs) {
             element.bind('keydown keypress', function (event) {
+                // Stop the cursor from bouncing around.
+                if (event.which === 38) {
+                    event.preventDefault();
+                }
                 if (event.which === 13) {
                     scope.$apply(function () {
                         scope.$eval(attrs.ngEnter, {$event:event});
