ac077b938e98a0da030fc72b4bb25280856d28eb
Lasse Fister <commander@graphicore.de>
2015-11-10 04:52:46 +0100

    chrome has problems loading from the live serve, testing configuration ...

--- red-pill-demo/redPillMain.js
+++ red-pill-demo/redPillMain.js
@@ -86552,6 +86552,7 @@ define("redPillMain", function(){});
 
 requirejs.config({
     baseUrl: 'lib'
+  , waitSeconds: 7
   , paths: {
         'require/domReady': 'bower_components/requirejs-domready/domReady'
       , 'require/text': 'bower_components/requirejs-text/text'
